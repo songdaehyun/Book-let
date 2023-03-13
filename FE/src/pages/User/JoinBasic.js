5d62f9a9cd1879e6c7f17fdbb6a3116eb42f41f3
aeebbr <aeebbr@gmail.com>
2023-03-13 17:57:43 +0900

    Feat: Add 회원가입 기본정보 입력 페이지 유효성 검사 및 핸들러 함수 생성

--- FE/src/pages/User/JoinBasic.js
+++ FE/src/pages/User/JoinBasic.js
@@ -10,10 +10,120 @@ import checkIcon from "../../assets/icons/check-circle-icon.svg";
 
 function JoinBasic() {
 	// 아이디
+	const [id, setId] = useState("");
 	const [idCntValid, setIdCntValid] = useState("default");
+	const [idIncludeValid, setIdIncludeValid] = useState("default");
+	const [idDuplicationValid, setIdDuplicationValid] = useState("default");
+
+	// 닉네임
+	const [nickname, setNickname] = useState("");
+	const [nicknameCntValid, setNicknameCntValid] = useState("default");
+	const [nicknameDuplicationValid, setNicknameDuplicationValid] = useState("default");
+
+	// 이메일
+	const [email, setEmail] = useState("");
+	const [emailFormValid, setEmailFormValid] = useState("default");
+	const [emailDuplicationValid, setEmailDuplicationValid] = useState("default");
+
+	// 비밀번호
+	const [pw, setPw] = useState("");
+	const [pwCntValid, setPwCntValid] = useState("default");
+	const [pwIncludeValid, setPwIncludeValid] = useState("default");
 
 	// 비밀번호 확인
+	const [pwConfirm, setPwConfirm] = useState("");
 	const [pwSameValid, setPwSameValid] = useState("default");
+
+	const check = `<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 0C2.688 0 0 2.688 0 6C0 9.312 2.688 12 6 12C9.312 12 12 9.312 12 6C12 2.688 9.312 0 6 0ZM4.8 9L1.8 6L2.646 5.154L4.8 7.302L9.354 2.748L10.2 3.6L4.8 9Z" style="fill:#CED4DA"/>
+    </svg>`;
+
+	const hadleChangeId = (e) => {
+		setId(e.target.value);
+	};
+	const hadleChangeNickname = (e) => {
+		setNickname(e.target.value);
+	};
+	const hadleChangeEmail = (e) => {
+		setEmail(e.target.value);
+	};
+
+	const hadleChangePw = (e) => {
+		setPw(e.target.value);
+	};
+	const hadleChangePwConfirm = (e) => {
+		setPwConfirm(e.target.value);
+	};
+
+	// 유효성 검사
+	const idValidTest = (e) => {
+		var regId = /[a-zA-Z]/g;
+
+		if (id.length <= 12) {
+			setIdCntValid("success");
+		} else {
+			setIdCntValid("fail");
+		}
+		if (regId.test(id)) {
+			setIdIncludeValid("success");
+		} else {
+			setIdIncludeValid("fail");
+		}
+
+		if (true) {
+			setIdDuplicationValid("success");
+		}
+	};
+
+	const nicknameValidTest = (e) => {
+		if (nickname.length <= 12) {
+			setNicknameCntValid("success");
+		} else {
+			setNicknameCntValid("fail");
+		}
+
+		if (true) {
+			setNicknameDuplicationValid("success");
+		}
+	};
+
+	const emailValidTest = (e) => {
+		var regEmail = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+		if (regEmail.test(email)) {
+			setEmailFormValid("success");
+		} else {
+			setEmailFormValid("fail");
+		}
+
+		if (true) {
+			setEmailDuplicationValid("success");
+		}
+	};
+
+	const pwValidTest = (e) => {
+		var regPw = /[`~!@#$%^&*|\\\'\";:\/?]/gi;
+
+		if (8 <= pw.length <= 16) {
+			setPwCntValid("success");
+		} else {
+			setPwCntValid("fail");
+		}
+
+		if (regPw.test(pw)) {
+			setPwIncludeValid("success");
+		} else {
+			setPwIncludeValid("fail");
+		}
+	};
+
+	const pwConfirmValidTest = (e) => {
+		if (pw === pwConfirm) {
+			setPwSameValid("success");
+		} else {
+			setPwSameValid("fail");
+		}
+	};
 
 	return (
 		<>
@@ -32,20 +142,35 @@ function JoinBasic() {
 							아이디
 							<DefaultInput
 								placeholder="아이디를 입력해주세요"
+								value={id}
+								onChange={hadleChangeId}
+								onBlur={idValidTest}
 								marginTop="8"
 								marginBottom="8"
 							></DefaultInput>
 						</Label>
 						<ValidLabelContainer>
 							<ValidLabel state={idCntValid}>
-								<img src={checkIcon} />
+								{/* <img src={checkIcon} /> */}
+								{/* <svg
+									width="12"
+									height="12"
+									viewBox="0 0 12 12"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M6 0C2.688 0 0 2.688 0 6C0 9.312 2.688 12 6 12C9.312 12 12 9.312 12 6C12 2.688 9.312 0 6 0ZM4.8 9L1.8 6L2.646 5.154L4.8 7.302L9.354 2.748L10.2 3.6L4.8 9Z"
+									/>
+								</svg> */}
+								dangerouslySetInnerHTML={{ __html: check }}
 								<span>12자 이내</span>
 							</ValidLabel>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={idIncludeValid}>
 								<img src={checkIcon} />
 								<span>영문 포함</span>
 							</ValidLabel>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={idDuplicationValid}>
 								<img src={checkIcon} />
 								<span>중복 확인</span>
 							</ValidLabel>
@@ -57,16 +182,19 @@ function JoinBasic() {
 							닉네임
 							<DefaultInput
 								placeholder="닉네임을 입력해주세요"
+								value={nickname}
+								onChange={hadleChangeNickname}
+								onBlur={nicknameValidTest}
 								marginTop="8"
 								marginBottom="8"
 							></DefaultInput>
 						</Label>
 						<ValidLabelContainer>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={nicknameCntValid}>
 								<img src={checkIcon} />
 								<span>12자 이내</span>
 							</ValidLabel>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={nicknameDuplicationValid}>
 								<img src={checkIcon} />
 								<span>중복 확인</span>
 							</ValidLabel>
@@ -78,16 +206,19 @@ function JoinBasic() {
 							이메일
 							<DefaultInput
 								placeholder="이메일을 입력해주세요"
+								value={email}
+								onChange={hadleChangeEmail}
+								onBlur={emailValidTest}
 								marginTop="8"
 								marginBottom="8"
 							></DefaultInput>
 						</Label>
 						<ValidLabelContainer>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={emailFormValid}>
 								<img src={checkIcon} />
 								<span>이메일 형식</span>
 							</ValidLabel>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={emailDuplicationValid}>
 								<img src={checkIcon} />
 								<span>중복 확인</span>
 							</ValidLabel>
@@ -99,16 +230,19 @@ function JoinBasic() {
 							비밀번호
 							<DefaultInput
 								placeholder="비밀번호를 입력해주세요"
+								value={pw}
+								onChange={hadleChangePw}
+								onBlur={pwValidTest}
 								marginTop="8"
 								marginBottom="8"
 							></DefaultInput>
 						</Label>
 						<ValidLabelContainer>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={pwCntValid}>
 								<img src={checkIcon} />
 								<span>8-16자 이내</span>
 							</ValidLabel>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={pwIncludeValid}>
 								<img src={checkIcon} />
 								<span>특수문자 포함</span>
 							</ValidLabel>
@@ -120,12 +254,15 @@ function JoinBasic() {
 							비밀번호 확인
 							<DefaultInput
 								placeholder="비밀번호를 재입력해주세요"
+								value={pwConfirm}
+								onChange={hadleChangePwConfirm}
+								onBlur={pwConfirmValidTest}
 								marginTop="8"
 								marginBottom="8"
 							></DefaultInput>
 						</Label>
 						<ValidLabelContainer>
-							<ValidLabel state={idCntValid}>
+							<ValidLabel state={pwSameValid}>
 								<img src={checkIcon} />
 								<span>비밀번호 일치</span>
 							</ValidLabel>
