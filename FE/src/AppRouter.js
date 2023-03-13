1a6d4569edb2955b13dfc0f02557c018d39ac2d2
aeebbr <aeebbr@gmail.com>
2023-03-13 15:33:29 +0900

    Feat: Add JoinBasic 라우팅

--- FE/src/AppRouter.js
+++ FE/src/AppRouter.js
@@ -3,16 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // 로그인 / 회원가입
 import Login from "./pages/User/Login";
+import JoinBasic from "./pages/User/JoinBasic";
 
 function AppRouter(props) {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* 로그인 / 회원가입 */}
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* 로그인 / 회원가입 */}
+				<Route path="/login" element={<Login />} />
+				<Route path="/join/1" element={<JoinBasic />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default AppRouter;
