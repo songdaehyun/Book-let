a36c5b5783710cd33f70dad120eb05b630ec660e
aeebbr <aeebbr@gmail.com>
2023-03-13 16:58:55 +0900

    Design: Add ValidLabel 컴포넌트 생성

--- FE/src/styles/common/CommonStyle.js
+++ FE/src/styles/common/CommonStyle.js
@@ -1,1 +1,25 @@
 import styled from "styled-components";
+
+export const ValidLabel = styled.div`
+	margin-right: 12px;
+
+	display: flex;
+	align-items: center;
+
+	img {
+		width: 12px;
+		height: 12px;
+
+		margin-right: 4px;
+	}
+
+	span {
+		font-size: 12px;
+		color: ${(props) =>
+			props.state === "success"
+				? "var(--primary-600)"
+				: props.state === "fail"
+				? "var(--red)"
+				: "var(--gray-500)"};
+	}
+`;
