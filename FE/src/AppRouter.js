9b106a04daf11f50382dcb72db091cdf9573984a
aeebbr <aeebbr@gmail.com>
2023-03-13 11:30:48 +0900

    Feat: Add 로그인 페이지 라우팅

--- FE/src/AppRouter.js
+++ FE/src/AppRouter.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// 로그인 / 회원가입
+import Login from "./pages/User/Login";
+
 function AppRouter(props) {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        {/* 로그인 / 회원가입 */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
