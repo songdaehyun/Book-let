0779be53508664c06adbc611a492f5d68032c22b
aeebbr <aeebbr@gmail.com>
2023-03-13 14:28:35 +0900

    Design: Add PrimaryLargeBtn, TextBtn 생성

---
+++ FE/src/styles/common/ButtonsStyle.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const PrimaryLargeBtn = styled.button`
+  width: 100%;
+
+  margin-top: ${(props) => props.marginTop}px;
+  margin-bottom: ${(props) => props.marginBottom}px;
+  padding: 12px 24px;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: var(--primary-600);
+  color: white;
+  font-weight: bold;
+
+  border-radius: 4px;
+  border: none;
+`;
+
+export const TextBtn = styled.button`
+  font-size: ${(props) => props.size}px;
+  color: var(--gray-500);
+
+  border: none;
+  background: none;
+`;
