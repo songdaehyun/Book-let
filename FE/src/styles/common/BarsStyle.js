4568d9a792cf78400ad02ec11ec03b34597aab88
aeebbr <aeebbr@gmail.com>
2023-03-13 16:58:32 +0900

    Design: Add ActionsNavigationBar 컴포넌트 생성

---
+++ FE/src/styles/common/BarsStyle.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const ActionsNavigationBar = styled.div`
+	width: 100%;
+	height: 56px;
+
+	padding: 16px;
+
+	display: flex;
+	justify-content: center;
+	position: fixed;
+	text-align: center;
+
+	background-color: white;
+
+	> button {
+		border: none;
+		background: none;
+	}
+
+	> button:first-child {
+		margin-right: auto;
+		float: left;
+	}
+
+	> span {
+		line-height: 24px;
+	}
+
+	> button:last-child {
+		margin-left: auto;
+		float: right;
+		color: var(--primary-600);
+		font-weight: bold;
+	}
+`;
