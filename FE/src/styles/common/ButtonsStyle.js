a7d4d1416fa0915e621d4826937dcdb0e5ca8229
aeebbr <aeebbr@gmail.com>
2023-03-13 15:25:57 +0900

    Design: Add PrimaryLargeBtn active state 추가

--- FE/src/styles/common/ButtonsStyle.js
+++ FE/src/styles/common/ButtonsStyle.js
@@ -1,27 +1,31 @@
 import styled from "styled-components";
 
 export const PrimaryLargeBtn = styled.button`
-  width: 100%;
+	width: 100%;
 
-  margin-top: ${(props) => props.marginTop}px;
-  margin-bottom: ${(props) => props.marginBottom}px;
-  padding: 12px 24px;
+	margin-top: ${(props) => props.marginTop}px;
+	margin-bottom: ${(props) => props.marginBottom}px;
+	padding: 12px 24px;
 
-  justify-content: center;
-  align-items: center;
+	justify-content: center;
+	align-items: center;
 
-  background-color: var(--primary-600);
-  color: white;
-  font-weight: bold;
+	background-color: var(--primary-600);
+	color: white;
+	font-weight: bold;
 
-  border-radius: 4px;
-  border: none;
+	border-radius: 4px;
+	border: none;
+
+	:active {
+		background-color: var(--primary-700);
+	}
 `;
 
 export const TextBtn = styled.button`
-  font-size: ${(props) => props.size}px;
-  color: var(--gray-500);
+	font-size: ${(props) => props.size}px;
+	color: var(--gray-500);
 
-  border: none;
-  background: none;
+	border: none;
+	background: none;
 `;
