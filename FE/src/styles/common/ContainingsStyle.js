a7c0b658afc8a30bcceaf27cba704276d67da4cc
aeebbr <aeebbr@gmail.com>
2023-03-13 16:59:49 +0900

    Design: Add ValidLabelContainer 컴포넌트 생성, Modify Container 수정

--- FE/src/styles/common/ContainingsStyle.js
+++ FE/src/styles/common/ContainingsStyle.js
@@ -5,8 +5,17 @@ export const Container = styled.div`
 	padding-bottom: ${(props) => props.paddingBottom}px;
 	padding-left: ${(props) => props.paddingLeft}px;
 	padding-right: ${(props) => props.paddingRight}px;
+
+	margin-top: ${(props) => props.marginTop}px;
+	margin-bottom: ${(props) => props.marginBottom}px;
+	margin-left: ${(props) => props.marginLeft}px;
+	margin-right: ${(props) => props.marginRight}px;
 `;
 
 export const ValidWrapper = styled.div`
 	margin-top: 4px;
+`;
+
+export const ValidLabelContainer = styled.div`
+	display: flex;
 `;
