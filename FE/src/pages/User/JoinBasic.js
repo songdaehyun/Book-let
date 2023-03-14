9d3e7b75d8a4e6836c82dacfa74509f578a42f90
aeebbr <aeebbr@gmail.com>
2023-03-14 09:50:16 +0900

    Feat: Modify ActionsNavigationBar 컴포넌트 적용

--- FE/src/pages/User/JoinBasic.js
+++ FE/src/pages/User/JoinBasic.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
+import { useNavigate } from "react-router-dom";
+
+import ActionsNavigationBar from "../../components/common/ActionsNavigationBar";
 import CricleCheck from "../../components/common/CricleCheck";
 
 import { ValidLabel } from "../../styles/common/CommonStyle";
 import { Container, ValidLabelContainer } from "../../styles/common/ContainingsStyle";
-import { ActionsNavigationBar } from "../../styles/common/BarsStyle";
 import { Text, Label } from "../../styles/common/TextsStyle";
 import { DefaultInput } from "../../styles/common/InputsStyle";
 
 function JoinBasic() {
+	const navigate = useNavigate();
+
 	// 아이디
 	const [id, setId] = useState("");
 	const [idCntValid, setIdCntValid] = useState("default");
@@ -43,7 +47,6 @@ function JoinBasic() {
 	const hadleChangeEmail = (e) => {
 		setEmail(e.target.value);
 	};
-
 	const hadleChangePw = (e) => {
 		setPw(e.target.value);
 	};
@@ -55,80 +58,129 @@ function JoinBasic() {
 	const idValidTest = (e) => {
 		var regId = /[a-zA-Z]/g;
 
+		// 아이디 유효성 패스 여부
+		let isValidConfirm = true;
+
 		if (0 < id.length && id.length <= 12) {
-			console.log(id.length);
 			setIdCntValid("success");
 		} else {
 			setIdCntValid("fail");
+			isValidConfirm = false;
 		}
 		if (regId.test(id)) {
 			setIdIncludeValid("success");
 		} else {
 			setIdIncludeValid("fail");
+			isValidConfirm = false;
 		}
 
 		if (true) {
 			setIdDuplicationValid("success");
 		}
+
+		return isValidConfirm;
 	};
 
 	const nicknameValidTest = (e) => {
+		// 닉네임 유효성 패스 여부
+		let isValidConfirm = true;
+
 		if (0 < nickname.length && nickname.length <= 12) {
 			setNicknameCntValid("success");
 		} else {
 			setNicknameCntValid("fail");
+			isValidConfirm = false;
 		}
 
 		if (true) {
 			setNicknameDuplicationValid("success");
 		}
+
+		return isValidConfirm;
 	};
 
 	const emailValidTest = (e) => {
 		var regEmail = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
 
+		// 이메일 유효성 패스 여부
+		let isValidConfirm = true;
+
 		if (regEmail.test(email)) {
 			setEmailFormValid("success");
 		} else {
 			setEmailFormValid("fail");
+			isValidConfirm = false;
 		}
 
 		if (true) {
 			setEmailDuplicationValid("success");
 		}
+
+		return isValidConfirm;
 	};
 
 	const pwValidTest = (e) => {
 		var regPw = /[`~!@#$%^&*|\\\'\";:\/?]/gi;
 
+		// 비밀번호 유효성 패스 여부
+		let isValidConfirm = true;
+
 		if (8 <= pw.length && pw.length <= 16) {
 			setPwCntValid("success");
 		} else {
 			setPwCntValid("fail");
+			isValidConfirm = false;
 		}
 
 		if (regPw.test(pw)) {
 			setPwIncludeValid("success");
 		} else {
 			setPwIncludeValid("fail");
+			isValidConfirm = false;
 		}
+
+		return isValidConfirm;
 	};
 
 	const pwConfirmValidTest = (e) => {
+		// 비밀번호 확인 유효성 패스 여부
+		let isValidConfirm = true;
+
 		if (pw === pwConfirm) {
 			setPwSameValid("success");
 		} else {
 			setPwSameValid("fail");
+			isValidConfirm = false;
+		}
+
+		return isValidConfirm;
+	};
+
+	const handleClickPre = () => {
+		navigate("/login");
+	};
+
+	const handleClickNext = () => {
+		if (
+			idValidTest() &&
+			nicknameValidTest() &&
+			emailValidTest() &&
+			pwValidTest() &&
+			pwConfirmValidTest()
+		) {
+			navigate("/login");
 		}
 	};
 
 	return (
 		<>
-			<ActionsNavigationBar>
-				<button>취소</button>
-				<span>회원가입</span>
-				<button>다음</button>
-			</ActionsNavigationBar>
+			<ActionsNavigationBar
+				pre="취소"
+				title="회원가입"
+				next="다음"
+				preAction={handleClickPre}
+				nextAction={handleClickNext}
+			/>
 			<Container paddingTop="56" paddingLeft="16" paddingRight="16">
 				<Text size="20" weight="bold">
 					기본 정보를 입력해주세요
@@ -239,6 +291,7 @@ function JoinBasic() {
 						<Label size="14" weight="600">
 							비밀번호 확인
 							<DefaultInput
+								type="password"
 								placeholder="비밀번호를 재입력해주세요"
 								value={pwConfirm}
 								onChange={hadleChangePwConfirm}
