6b2e2b4f5c026893d4a1904ced2bec70f6218f91
aeebbr <aeebbr@gmail.com>
2023-03-14 09:48:43 +0900

    Feat: Add ActionsNavigationBar 컴포넌트화

---
+++ FE/src/components/common/ActionsNavigationBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { ActionsNavigationBarWrapper } from "../../styles/common/BarsStyle";
+
+function ActionsNavigationBar({ pre, title, next, preAction, nextAction }) {
+	return (
+		<ActionsNavigationBarWrapper>
+			<button onClick={preAction}>{pre}</button>
+			<span>{title}</span>
+			<button onClick={nextAction}>{next}</button>
+		</ActionsNavigationBarWrapper>
+	);
+}
+
+export default ActionsNavigationBar;
