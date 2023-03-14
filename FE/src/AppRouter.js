e92c51df34483dfc7b7e62a36a1fc70b0c8c25c7
aeebbr <aeebbr@gmail.com>
2023-03-14 11:26:02 +0900

    Feat: Add JoinAge 라우팅

--- FE/src/AppRouter.js
+++ FE/src/AppRouter.js
@@ -4,7 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // 로그인 / 회원가입
 import Login from "./pages/User/Login";
 import JoinBasic from "./pages/User/JoinBasic";
-import JoinAge from "./pages/User/JoinAge";
+import JoinGender from "./pages/User/JoinGender";
+import JoinAge from "./pages/User/JoinGender";
 
 function AppRouter(props) {
 	return (
@@ -13,7 +14,8 @@ function AppRouter(props) {
 				{/* 로그인 / 회원가입 */}
 				<Route path="/login" element={<Login />} />
 				<Route path="/join/1" element={<JoinBasic />} />
-				<Route path="/join/2" element={<JoinAge />} />
+				<Route path="/join/2" element={<JoinGender />} />
+				<Route path="/join/3" element={<JoinAge />} />
 			</Routes>
 		</BrowserRouter>
 	);
