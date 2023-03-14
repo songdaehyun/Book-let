be3dcc3d33ecbb04e3cdf96523810dbf32fe20a4
aeebbr <aeebbr@gmail.com>
2023-03-14 17:25:59 +0900

    Feat: Modify ActionsNavigationBar nextColor 속성 추가

--- FE/src/components/common/ActionsNavigationBar.js
+++ FE/src/components/common/ActionsNavigationBar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import { ActionsNavigationBarWrapper } from "../../styles/common/BarsStyle";
 
-function ActionsNavigationBar({ pre, title, next, preAction, nextAction }) {
+function ActionsNavigationBar({ pre, title, next, preAction, nextAction, nextColor }) {
 	return (
-		<ActionsNavigationBarWrapper>
+		<ActionsNavigationBarWrapper nextColor={nextColor}>
 			<button onClick={preAction}>{pre}</button>
 			<span>{title}</span>
 			<button onClick={nextAction}>{next}</button>
