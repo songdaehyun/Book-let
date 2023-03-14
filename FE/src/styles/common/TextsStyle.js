7b85c0a7d46b76bceee79af8079aa513adfc334e
aeebbr <aeebbr@gmail.com>
2023-03-14 17:28:54 +0900

    Design: Add Span 스타일 생성

--- FE/src/styles/common/TextsStyle.js
+++ FE/src/styles/common/TextsStyle.js
@@ -11,6 +11,17 @@ export const Text = styled.div`
 	color: ${(props) => props.color};
 `;
 
+export const Span = styled.span`
+	margin-top: ${(props) => props.marginTop}px;
+	margin-bottom: ${(props) => props.marginBottom}px;
+	margin-left: ${(props) => props.marginLeft}px;
+	margin-right: ${(props) => props.marginRight}px;
+
+	font-size: ${(props) => props.size}px;
+	font-weight: ${(props) => props.weight};
+	color: ${(props) => props.color};
+`;
+
 export const ValidFailText = styled.span`
 	font-size: 14px;
 	color: var(--red);
