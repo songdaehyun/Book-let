907ceed9b98407821be6818f2fcda715b528cd12
aeebbr <aeebbr@gmail.com>
2023-03-14 20:47:58 +0900

    Feat: Add EmotionTag 컴포넌트  생성

---
+++ FE/src/components/user/EmotionTag.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Span, Text } from "../../styles/common/TextsStyle";
+import { TagWrapper } from "../../styles/User/JoinStyle";
+
+function EmotionTag({ id, title, isSelected }) {
+	return (
+		<TagWrapper isSelected={isSelected}>
+			<Span color={isSelected ? "white" : "var(--primary-600)"} weight="bold">
+				#
+			</Span>
+			<Text color={isSelected && "white"} marginLeft="4">
+				{title}
+			</Text>
+		</TagWrapper>
+	);
+}
+
+export default EmotionTag;
