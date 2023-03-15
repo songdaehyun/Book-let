43af979f8a824b85d40eff0b42351b2e25c1bca8
aeebbr <aeebbr@gmail.com>
2023-03-15 16:17:31 +0900

    Feat: Add Preview Scrap, Comment 탭에 따른 조건부 렌더링

--- FE/src/components/feed/post/PreivewScrap.js
+++ FE/src/components/feed/post/PreivewScrap.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Text } from "../../../styles/common/TextsStyle";
-import { PreviewPostInfo } from "../../../styles/feed/PostPreviewStyle";
-import ScrapInfo from "../../common/ScrapInfo";
 
-function PreivewScrap({ post }) {
+import ScrapInfo from "../../common/ScrapInfo";
+import ScrapButton from "../../common/ScrapButton";
+
+import { PreviewPostInfo } from "../../../styles/feed/PostPreviewStyle";
+import { Text } from "../../../styles/common/TextsStyle";
+
+function PreivewScrap({ post, isMy }) {
 	return (
 		<PreviewPostInfo>
 			<ScrapInfo post={post} />
-			<Text size="14" color="var(--gray-500)">
-				{post.createdDate}
-			</Text>
+			{isMy ? (
+				<Text size="14" color="var(--gray-500)">
+					{post.createdDate}
+				</Text>
+			) : (
+				<ScrapButton isScraped={post.userScrapted} />
+			)}
 		</PreviewPostInfo>
 	);
 }
--- FE/src/components/feed/post/PreviewComment.js
+++ FE/src/components/feed/post/PreviewComment.js
@@ -4,16 +4,23 @@ import { PreviewCommentContainer } from "../../../styles/feed/PostPreviewStyle";
 
 import commentIcon from "../../../assets/icons/comment-icon.svg";
 
-function PreviewComment({ post }) {
+function PreviewComment({ post, isMy }) {
 	return (
 		<PreviewCommentContainer>
-			<img src={commentIcon} />
-			<Text size="14" color="var(--gray-500)">
-				<Span size="14" weight="bold">
-					{post.commentCnt}
-				</Span>
-				개의 댓글
-			</Text>
+			<div>
+				<img src={commentIcon} />
+				<Text size="14" color="var(--gray-500)">
+					<Span size="14" weight="bold">
+						{post.commentCnt}
+					</Span>
+					개의 댓글
+				</Text>
+			</div>
+			{!isMy && (
+				<Text size="14" color="var(--gray-500)">
+					{post.createdDate}
+				</Text>
+			)}
 		</PreviewCommentContainer>
 	);
 }
