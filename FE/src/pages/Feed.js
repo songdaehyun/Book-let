899274cea384407df7501d51e92918d8d5f9667a
aeebbr <aeebbr@gmail.com>
2023-03-15 14:30:13 +0900

    Feat: Add Feed 마크업

--- FE/src/pages/Feed.js
+++ FE/src/pages/Feed.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import FeedExplore from "../components/feed/FeedExploreTab";
+import FeedMyTab from "../components/feed/FeedMyTab";
+
+import FeedTab from "../components/feed/FeedTab";
+
+import { Container } from "../styles/common/ContainingsStyle";
+import { Text } from "../styles/common/TextsStyle";
 
 function Feed(props) {
-	return <div></div>;
+	const [selectedItem, setSelectedItem] = useState(1);
+	return (
+		<Container paddingTop="24" paddingLeft="16" paddingRight="16">
+			<Text size="24" weight="bold" marginBottom="24">
+				Feed
+			</Text>
+			<FeedTab selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
+			{selectedItem === 1 ? <FeedMyTab /> : <FeedExplore />}
+		</Container>
+	);
 }
 
 export default Feed;
