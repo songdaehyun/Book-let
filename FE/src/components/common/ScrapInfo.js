7ff8a1343a8e35f4b93dfee61139164df339b7c0
aeebbr <aeebbr@gmail.com>
2023-03-15 14:54:45 +0900

    Feat: Add ScrapInfo 컴포넌트 구현

--- FE/src/components/common/ScrapInfo.js
+++ FE/src/components/common/ScrapInfo.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Span } from "../../styles/common/TextsStyle";
+import {
+	ScrapContainer,
+	ScrapImgContainer,
+	ScrapImgWrapper,
+} from "../../styles/feed/PostPreviewStyle";
 
-function ScrapInfo(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function ScrapInfo({ post }) {
+	return (
+		<>
+			<ScrapImgContainer>
+				{post.scrapUserImgs.map((img, idx) => (
+					<ScrapImgWrapper key={idx}>
+						<img src={img} />
+					</ScrapImgWrapper>
+				))}
+			</ScrapImgContainer>
+			<ScrapContainer>
+				{/* <div> */}
+					<Span weight="bold" color="var(--primary-600)">
+						{post.scrapCnt}
+					</Span>
+					명이 스크랩했어요
+				{/* </div> */}
+			</ScrapContainer>
+		</>
+	);
 }
 
 export default ScrapInfo;
