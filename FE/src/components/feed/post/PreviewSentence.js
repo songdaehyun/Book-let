1fb92973ee45df46d3128801a1fa62a2fb195183
aeebbr <aeebbr@gmail.com>
2023-03-15 14:53:45 +0900

    Feat: Add PreviewSentence 컴포넌트 구현

--- FE/src/components/feed/post/PreviewSentence.js
+++ FE/src/components/feed/post/PreviewSentence.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Text } from "../../../styles/common/TextsStyle";
+import {
+	PreviewSentenceBottonContent,
+	PreviewSentenceContainer,
+	PreviewSentenceContent,
+} from "../../../styles/feed/PostPreviewStyle";
 
-function PreviewSentence(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function PreviewSentence({ post }) {
+	return (
+		<PreviewSentenceContainer color={post.paragraphColor}>
+			<PreviewSentenceContent>
+				<Text font="jeju" height="28">
+					{post.content}
+				</Text>
+				<PreviewSentenceBottonContent>
+					<div>
+						<Text size="12" weight="bold" marginBottom="4">
+							{post.bookTitle}
+						</Text>
+						<Text size="12">{post.bookAuthor}</Text>
+					</div>
+					<Text size="12">P. {post.paragraphPage}</Text>
+				</PreviewSentenceBottonContent>
+			</PreviewSentenceContent>
+		</PreviewSentenceContainer>
+	);
 }
 
 export default PreviewSentence;
