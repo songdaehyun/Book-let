b77c7869af12cf1d7b3545727827780def81e878
aeebbr <aeebbr@gmail.com>
2023-03-15 14:54:12 +0900

    Feat: Add PreviewScrap 컴포넌트 구현

--- FE/src/components/feed/post/PreivewScrap.js
+++ FE/src/components/feed/post/PreivewScrap.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Text } from "../../../styles/common/TextsStyle";
+import { PreviewPostInfo } from "../../../styles/feed/PostPreviewStyle";
+import ScrapInfo from "../../common/ScrapInfo";
 
-function PreivewScrap(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function PreivewScrap({ post }) {
+	return (
+		<PreviewPostInfo>
+			<ScrapInfo post={post} />
+			<Text size="14" color="var(--gray-500)">
+				{post.createdDate}
+			</Text>
+		</PreviewPostInfo>
+	);
 }
 
 export default PreivewScrap;
