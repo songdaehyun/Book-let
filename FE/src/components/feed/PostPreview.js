fe19fb04e096aa0146729e71e913d81cd5d5237c
aeebbr <aeebbr@gmail.com>
2023-03-15 14:55:24 +0900

    Feat: Add PostPreview 컴포넌트 구현

--- FE/src/components/feed/PostPreview.js
+++ FE/src/components/feed/PostPreview.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
-function PostPreview(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+import PreviewSentence from "./post/PreviewSentence";
+import PreviewComment from "./post/PreviewComment";
+import PreivewScrap from "./post/PreivewScrap";
+
+import { Container } from "../../styles/common/ContainingsStyle";
+
+function PostPreview({ post }) {
+	return (
+		<Container marginBottom="40">
+			<PreviewSentence post={post} />
+			<PreivewScrap post={post} />
+			<PreviewComment post={post} />
+		</Container>
+	);
 }
 
 export default PostPreview;
