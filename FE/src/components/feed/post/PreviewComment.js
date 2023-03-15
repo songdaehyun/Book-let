37ec57e5c3ae096611a9d73c629a28508103db5c
aeebbr <aeebbr@gmail.com>
2023-03-15 14:53:58 +0900

    Feat: Add PreviewComment 컴포넌트 구현

--- FE/src/components/feed/post/PreviewComment.js
+++ FE/src/components/feed/post/PreviewComment.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Span, Text } from "../../../styles/common/TextsStyle";
+import { PreviewCommentContainer } from "../../../styles/feed/PostPreviewStyle";
 
-function PreviewComment(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+import commentIcon from "../../../assets/icons/comment-icon.svg";
+
+function PreviewComment({ post }) {
+	return (
+		<PreviewCommentContainer>
+			<img src={commentIcon} />
+			<Text size="14" color="var(--gray-500)">
+				<Span size="14" weight="bold">
+					{post.commentCnt}
+				</Span>
+				개의 댓글
+			</Text>
+		</PreviewCommentContainer>
+	);
 }
 
 export default PreviewComment;
