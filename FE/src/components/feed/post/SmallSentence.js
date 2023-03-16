26e9c2e608e432699e4db1fd9698481c4800253e
aeebbr <aeebbr@gmail.com>
2023-03-16 09:37:29 +0900

    Feat: Add SmallSentence 마크업 및 구현

--- FE/src/components/feed/post/SmallSentence.js
+++ FE/src/components/feed/post/SmallSentence.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Text } from "../../../styles/common/TextsStyle";
+import { SmallSentenceContainer } from "../../../styles/feed/PreviewRecoStyle";
 
-function SmallSentence(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function SmallSentence({ sentence }) {
+	return (
+		<SmallSentenceContainer color={sentence.paragraphColor}>
+			<Text font="jeju" height="22">
+				{sentence.content}
+			</Text>
+		</SmallSentenceContainer>
+	);
 }
 
 export default SmallSentence;
