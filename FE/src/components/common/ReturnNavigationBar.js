ab38426e357b71646eb56a01efddd050cf47a2f0
aeebbr <aeebbr@gmail.com>
2023-03-16 13:15:33 +0900

    Feat: Add ReturnNavigationBar 아이콘 클릭 시 이전 페이지로 이동

--- FE/src/components/common/ReturnNavigationBar.js
+++ FE/src/components/common/ReturnNavigationBar.js
@@ -4,11 +4,19 @@ import { ReturnNavigationBarContainer } from "../../styles/common/BarsStyle";
 import { Text } from "../../styles/common/TextsStyle";
 
 import ArrowIcon from "../../assets/icons/left-arrow-icon.png";
+import { useNavigate } from "react-router-dom";
 
 function ReturnNavigationBar({ title }) {
+	const navigate = useNavigate();
+
+	const handleClickReturn = () => {
+		// 이전 페이지로 이동
+		navigate(-1);
+	};
+
 	return (
 		<ReturnNavigationBarContainer>
-			<div>
+			<div onClick={handleClickReturn}>
 				<img src={ArrowIcon} />
 			</div>
 			<Text>{title}</Text>
