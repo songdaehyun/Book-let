38b51d333e1035c949c0c7b7882006659d3a7e4e
전동준 <jdznawa@naver.com>
2023-03-16 17:38:42 +0900

    FIX: 테이블에 이미지 URL 저장하는 컬럼이 없는 문제 수정

--- BE/django/bookcover/models.py
+++ BE/django/bookcover/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 
 class BookInfoModel(models.Model):
-    book_id = models.AutoField(primary_key=True)
-    isdn = models.CharField(max_length=13)
-    cartoon = models.FloatField(default=0)
-    solid = models.FloatField(default=0)
-    infographic = models.FloatField(default=0)
-    realistic = models.FloatField(default=0)
-    art = models.FloatField(default=0)
+    book_id = models.AutoField(primary_key=True)    # 책 id
+    isdn = models.CharField(max_length=13)  # ISDN(13자리)
+    imgsrc = models.CharField(max_length=300)   # 이미지 출처(URL)
+    cartoon = models.FloatField(default=0)  # 만화 그림체 유사도
+    solid = models.FloatField(default=0)    # 솔리드 그림체 유사도
+    infographic = models.FloatField(default=0)  # 인포그래픽(단색, 추상화) 그림체 유사도
+    realistic = models.FloatField(default=0)    # 사실 묘사 그림체(사진 포함) 유사도
+    art = models.FloatField(default=0)  # 예술(일러스트 포함) 그림체 유사도
 
     class Meta:
         db_table = 'bookinfo'
