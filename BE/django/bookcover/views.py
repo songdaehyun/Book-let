db91bdfb0357964b39857cf811bec883b5ad957f
전동준 <jdznawa@naver.com>
2023-03-16 18:03:41 +0900

    FEAT: Django 서버 환경설정 추가

--- BE/django/bookcover/views.py
+++ BE/django/bookcover/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import torch
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+# .pt로 저장된 모델 불러오기
+model = torch.load('../model/test300.pt')
+model.eval()
+
+
+@csrf_exempt
+def image_recommend(request):   # 예측 기능 수행
+    if request.method == 'POST':
+        # Get input data from POST request
+        input_data = request.POST.get('input_data')
+
+        # Prepare the input data for the model
+        # ...
+
+        # Pass the input data to the model
+        output = model(input_data)
+
+        # Format the output as JSON
+        response = {'output': output.tolist()}
+
+        # Return the response as a JSON object
+        return JsonResponse(response)
