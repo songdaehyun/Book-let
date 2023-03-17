f14d048f43f7015447d715c40d03af89dd1b54aa
aeebbr <aeebbr@gmail.com>
2023-03-17 17:10:01 +0900

    Feat: Add DetailSentence 라우팅

--- FE/src/AppRouter.js
+++ FE/src/AppRouter.js
@@ -9,10 +9,11 @@ import JoinAge from "./components/pages/Join/JoinAge";
 import JoinCover from "./components/pages/Join/JoinCover";
 import JoinTag from "./components/pages/Join/JoinTag";
 
-// 피드
+// 문장
 import Feed from './components/pages/Sentence/Feed'
 import Scrap from "./components/pages/Sentence/Scrap";
 import Reco from "./components/pages/Sentence/Reco";
+import DetailSentence from "./components/pages/Sentence/DetailSentence";
 
 function AppRouter(props) {
 	return (
@@ -25,10 +26,11 @@ function AppRouter(props) {
 				<Route path="/join/3" element={<JoinAge />} />
 				<Route path="/join/4" element={<JoinCover />} />
 				<Route path="/join/5" element={<JoinTag />} />
-				{/* 피드 */}
+				{/* 문장 */}
 				<Route path="/" element={<Feed />} />
 				<Route path="/sentence/scrap" element={<Scrap />} />
 				<Route path="/sentence/recommand" element={<Reco />} />
+				<Route path="/sentence/:sId" element={<DetailSentence />} />
 			</Routes>
 		</BrowserRouter>
 	);
