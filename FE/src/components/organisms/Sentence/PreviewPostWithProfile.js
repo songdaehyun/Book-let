2ac2997b0186005a06d4d771e855d2a57fc589e9
aeebbr <aeebbr@gmail.com>
2023-03-17 16:58:03 +0900

    Feat: Design: Add PreviewPostWithProfile 마크업

--- FE/src/components/organisms/Sentence/PreviewPostWithProfile.js
+++ FE/src/components/organisms/Sentence/PreviewPostWithProfile.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import PreviewProfile from "../../molecules/Sentence/PreviewProfile";
+import PreviewPost from "./PreviewPost";
 
-function PreviewPostWithProfile(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function PreviewPostWithProfile({ post }) {
+	return (
+		<>
+			<PreviewProfile nickname={post.nickname} profileImg={post.profileImg} />
+			<PreviewPost key={post.id} post={post} isMy={false} />
+		</>
+	);
 }
 
 export default PreviewPostWithProfile;
