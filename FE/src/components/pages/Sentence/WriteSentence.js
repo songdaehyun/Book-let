346142a2d6653bfa1edb7ce86ad3ba96314e18f7
aeebbr <aeebbr@gmail.com>
2023-03-19 08:39:38 +0900

    Feat: Add WriteSentence 생성

---
+++ FE/src/components/pages/Sentence/WriteSentence.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SentenceForm from '../../organisms/Sentence/SentenceForm';
+
+function WriteSentence(props) {
+    return (
+        <div>
+            <SentenceForm />
+        </div>
+    );
+}
+
+export default WriteSentence;
