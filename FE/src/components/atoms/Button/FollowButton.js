9da00aa030f54d79578c9f7c3bddb7ecd42c3052
aeebbr <aeebbr@gmail.com>
2023-03-19 07:38:13 +0900

    Design: Add FollowButton 마크업

--- FE/src/components/atoms/Button/FollowButton.js
+++ FE/src/components/atoms/Button/FollowButton.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { FollowBtn } from "../../../styles/common/ButtonsStyle";
 
-function FollowButton(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function FollowButton({ isFollowed, setIsFollowed }) {
+	const handleClickFollow = () => {
+        setIsFollowed(!isFollowed);
+	};
+
+	return (
+		<FollowBtn onClick={handleClickFollow} isFollowed={isFollowed}>
+			{isFollowed ? <>언팔로우</> : <>팔로우</>}
+		</FollowBtn>
+	);
 }
 
 export default FollowButton;
--- FE/src/styles/common/ButtonsStyle.js
+++ FE/src/styles/common/ButtonsStyle.js
@@ -22,6 +22,28 @@ export const PrimaryLargeBtn = styled.button`
 	}
 `;
 
+export const PrimarySmBtn = styled.button`
+	height: fit-content;
+
+	margin-top: ${(props) => props.marginTop}px;
+	margin-bottom: ${(props) => props.marginBottom}px;
+	padding: 8px 16px;
+
+	justify-content: center;
+	align-items: center;
+
+	background-color: var(--primary-600);
+	color: white;
+	font-weight: bold;
+
+	border-radius: 4px;
+	border: none;
+
+	:active {
+		background-color: var(--primary-700);
+	}
+`;
+
 export const TextBtn = styled.button`
 	font-size: ${(props) => props.size}px;
 	color: var(--gray-500);
@@ -32,4 +54,35 @@ export const TextBtn = styled.button`
 
 export const ScrapBtn = styled.svg`
 	fill: ${(props) => (props.isScraped ? "var(--primary-600)" : "var(--gray-400)")};
+`;
+
+export const FollowBtn = styled(PrimarySmBtn)`
+	background-color: ${(props) => (props.isFollowed ? "var(--gray-100)" : "var(--secondary-200)")};
+	color: ${(props) => (props.isFollowed ? "black" : "var(--primary-700)")};
+
+	:active {
+		background-color: ${(props) =>
+			props.isFollowed ? "var(--gray-200)" : "var(--secondary-300)"};
+	}
+`;
+
+export const CommentUploadBtn = styled.button`
+	width: 32px;
+	height: 32px;
+
+	margin-left: 16px;
+
+	border: 0.8px solid var(--gray-300);
+
+	display: flex;
+	border-radius: 50%;
+
+	justify-content: center;
+	align-items: center;
+
+	background-color: white;
+
+	:active {
+		background-color: var(--gray-50);
+	}
 `;
