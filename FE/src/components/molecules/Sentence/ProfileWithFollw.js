675bac1a312f33640054f259a8130f5d12bce751
aeebbr <aeebbr@gmail.com>
2023-03-18 18:59:27 +0900

    Rename: DetailSentenceProfile -> ProfileWithFollow

---
+++ FE/src/components/molecules/Sentence/ProfileWithFollw.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SentenceProfile from "./SentenceProfile";
+
+function ProfileWithFollw(props) {
+	return (
+		<div>
+			<SentenceProfile />
+		</div>
+	);
+}
+
+export default ProfileWithFollw;
