edbab2280d351c7dc993a0924a003da75d020bad
aeebbr <aeebbr@gmail.com>
2023-03-20 02:28:13 +0900

    Design: Add WriteSentence에 ActionsNavigationBar 추가

--- FE/src/components/pages/Sentence/WriteSentence.js
+++ FE/src/components/pages/Sentence/WriteSentence.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import SentenceForm from '../../organisms/Sentence/SentenceForm';
+import React from "react";
+
+import ActionsNavigationBar from "../../molecules/Bar/ActionsNavigationBar";
+import SentenceForm from "../../organisms/Sentence/SentenceForm";
 
 function WriteSentence(props) {
-    return (
-        <div>
-            <SentenceForm />
-        </div>
-    );
+	return (
+		<>
+			<ActionsNavigationBar pre="취소" title="문장 쓰기" next="확인" />
+			<SentenceForm />
+		</>
+	);
 }
 
 export default WriteSentence;
