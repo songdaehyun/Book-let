cc7e4f03be9290cd781c8305d412323486d10077
aeebbr <aeebbr@gmail.com>
2023-03-20 09:30:02 +0900

    Feat: Add 문장 쓰기 ActionNavigationBar 클릭 핸들러 함수 props로 전달

--- FE/src/components/pages/Sentence/WriteSentence.js
+++ FE/src/components/pages/Sentence/WriteSentence.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import ActionsNavigationBar from "../../molecules/Bar/ActionsNavigationBar";
 import SentenceForm from "../../organisms/Sentence/SentenceForm";
 
 function WriteSentence(props) {
+	const navigate = useNavigate();
+
+	const handleClickPre = () => {
+		navigate("/");
+	};
+
+	const handleClickNext = () => {
+		// navigate("/");
+	};
+
 	return (
 		<>
-			<ActionsNavigationBar pre="취소" title="문장 쓰기" next="확인" />
+			<ActionsNavigationBar
+				pre="취소"
+				title="문장 쓰기"
+				next="확인"
+				handleClickPre={handleClickPre}
+				handleClickNext={handleClickNext}
+			/>
 			<SentenceForm />
 		</>
 	);
