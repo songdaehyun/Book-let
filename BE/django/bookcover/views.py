3b07f727a43da9cd5c151ad86a9bdc62b412da0f
전동준 <jdznawa@naver.com>
2023-03-20 11:22:26 +0900

    FIX: 환경설정 변경 사항 requirements.txt에 저장, views.py 코드 일부 수정

--- BE/django/bookcover/views.py
+++ BE/django/bookcover/views.py
@@ -28,9 +28,6 @@ def image_recommend(request):   # 예측 기능 수행
         input_info = model(input_pixel)
         input_info = input_info.pandas().xyxy[0]
 
-        image_class = []
-        image_confidence = []
-
         # image_spec = 이미지의 confidence 값을 df로 요약한 형태
         image_spec = pd.DataFrame(columns=[
             "book_id",
@@ -43,12 +40,14 @@ def image_recommend(request):   # 예측 기능 수행
             "art"
         ])
 
+        # image_value = 이미지 keyword의 confidence 값을 딕셔너리 형태로 정리
+        image_value = dict()
         # input_info 값을 하나씩 조회
         for col in range(input_info.shape[0]):
-            # image_spec에 해당하는 열이 있으면 confidence 값으로 대체
-            image_class.append(input_info.name[col])
-            image_confidence.append(input_info.confidence[col])
+            # 딕셔너리에 {분류: 유사도} 형태로 저장
+            image_value[input_info.name[col]] = input_info.confidence[col]
 
+        # imave_value 값 합치기
         # confidence 값이 없는 경우(결측치인 경우) 0으로 대체
         image_spec = image_spec.fillna(0)
 
