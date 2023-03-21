2723d8fb6699728cce85ac5bddea8eefcfd04838
aeebbr <aeebbr@gmail.com>
2023-03-21 11:05:29 +0900

    Design: Add 도서 추천 페이지를 템플릿으로 컴포넌트 분리 및 마크업

---
+++ FE/src/components/templates/Book/RecomBookListTemplates.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import TopBanner from "../../molecules/Banner/TopBanner";
+import ReturnNavigationBar from "../../molecules/Bar/ReturnNavigationBar";
+import BookList from "../../organisms/Book/BookList";
+import MasonryBookList from "../../organisms/Book/MasonryBookList";
+
+import { Container } from "../../../styles/common/ContainingsStyle";
+
+function RecomBookListTemplates({ title, subTitle, img, books, type }) {
+	return (
+		<div>
+			<ReturnNavigationBar />
+			<Container paddingTop="48">
+				<TopBanner title={title} subTitle={subTitle} img={img} />
+			</Container>
+			{type !== "bookCover" ? <BookList books={books} /> : <MasonryBookList books={books} />}
+		</div>
+	);
+}
+
+export default RecomBookListTemplates;
