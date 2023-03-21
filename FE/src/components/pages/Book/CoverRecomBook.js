50497adbb63bdd4fe773b4fef849cead59c84f65
aeebbr <aeebbr@gmail.com>
2023-03-21 11:06:56 +0900

    Design: Add CoverRecomBook 마크업

--- FE/src/components/pages/Book/CoverRecomBook.js
+++ FE/src/components/pages/Book/CoverRecomBook.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React from "react";
+
+import RecomBookListTemplates from "../../templates/Book/RecomBookListTemplates";
+
+import BannerImg from "../../../assets/images/Banner/cover-recom-book-banner.png";
 
 function CoverRecomBook(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	const books = {
+		recommendType: "bookCover",
+		recommend: [
+			{
+				bookImgPath: "http://image.yes24.com/goods/117317122/FRONT/XL",
+				bookTitle: "별의 커비 디스커버리 2",
+				authorNames: ["가리노 타우"],
+				bookIsbn: 9791164798957,
+			},
+			{
+				bookImgPath: "http://image.yes24.com/goods/117417122/FRONT/XL",
+				bookTitle: "별의 커비 디스커버리 2",
+				authorNames: ["가리노 타우"],
+				bookIsbn: 9791164798957,
+			},
+			{
+				bookImgPath: "http://image.yes24.com/goods/117327164/FRONT/XL",
+				bookTitle: "별의 커비 디스커버리 2",
+				authorNames: ["가리노 타우"],
+				bookIsbn: 9791164798957,
+			},
+			{
+				bookImgPath: "http://image.yes24.com/goods/117337122/FRONT/XL",
+				bookTitle: "별의 커비 디스커버리 2",
+				authorNames: ["가리노 타우"],
+				bookIsbn: 9791164798957,
+			},
+			{
+				bookImgPath: "http://image.yes24.com/goods/117327961/FRONT/XL",
+				bookTitle: "별의 커비 디스커버리 2",
+				authorNames: ["가리노 타우"],
+				bookIsbn: 9791164798957,
+			},
+		],
+	};
+
+	const bannerInfo = {
+		title: (
+			<>
+				좋아하실만한 표지의
+				<br /> 책이에요
+			</>
+		),
+		subTitle: (
+			<>
+				선호하시는 스타일의 표지를 위주로
+				<br />
+				도서를 추천해드려요.
+			</>
+		),
+		img: BannerImg,
+	};
+
+	return (
+		<RecomBookListTemplates
+			title={bannerInfo.title}
+			subTitle={bannerInfo.subTitle}
+			img={bannerInfo.img}
+			books={books.recommend}
+			type={books.recommendType}
+		/>
+	);
 }
 
 export default CoverRecomBook;
