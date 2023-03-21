24569ecea953f4ee94a6f3f4ea7aa7355b98f894
aeebbr <aeebbr@gmail.com>
2023-03-21 11:07:16 +0900

    Design: Add MansonryBookList 마크업

---
+++ FE/src/components/organisms/Book/MasonryBookList.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import {
+	ListBookCoverMasonryBox,
+	PreviewBookMasonryOverviewBox,
+} from "../../../styles/Book/BookStyle";
+import { Text } from "../../../styles/common/TextsStyle";
+
+function MasonryBookList({ books }) {
+	const navigate = useNavigate();
+
+	const handleClick = (isbn) => {
+		navigate(`/book/${isbn}`);
+	};
+
+	return (
+		<ListBookCoverMasonryBox>
+			{books.map((book) => {
+				return (
+					<PreviewBookMasonryOverviewBox key={book.bookIsbn}>
+						<img
+							src={book.bookImgPath}
+							alt="book"
+							onClick={() => handleClick(book.bookIsbn)}
+						/>
+						<Text
+							weight="600"
+							marginBottom="4"
+							onClick={() => handleClick(book.bookIsbn)}
+						>
+							{book.bookTitle}
+						</Text>
+						<Text size="14" color="var(--gray-500)">
+							{book.authorNames}
+						</Text>
+					</PreviewBookMasonryOverviewBox>
+				);
+			})}
+		</ListBookCoverMasonryBox>
+	);
+}
+
+export default MasonryBookList;
