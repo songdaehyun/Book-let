f6edc178d72c9e874641c457ec19df276e4697d1
aeebbr <aeebbr@gmail.com>
2023-03-21 16:27:23 +0900

    Design: Add 도서 개요 섹션 마크업

---
+++ FE/src/styles/Book/BookDetailStyle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const BookDetailOverviewBox = styled.div`
+	padding-top: 51px;
+
+	display: flex;
+	flex-direction: column;
+
+	> img {
+		width: 40%;
+		align-self: center;
+
+		margin-top: 16px;
+		margin-bottom: 40px;
+
+		filter: drop-shadow(0px 0px 15px rgba(0, 0, 0, 0.25));
+		border-radius: 0px;
+	}
+`;
