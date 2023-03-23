91ca867cb5f90e820f9ff26b4cefdf884156e464
aeebbr <aeebbr@gmail.com>
2023-03-23 10:32:45 +0900

    Feat: Add 마이페이지 좋아요 페이지 마크업

--- FE/src/components/pages/Mypage/MyLike.js
+++ FE/src/components/pages/Mypage/MyLike.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+
+import { Container } from "../../../styles/common/ContainingsStyle";
+import MyLikeHeading from "../../atoms/Mypage/MyLikeHeading";
+
+import ReturnNavigationBar from "../../molecules/Bar/ReturnNavigationBar";
+import BookList from "../../organisms/Book/BookList";
 
 function MyLike(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	const count = 15;
+	const books = [
+		{
+			bookIsbn: 9788901269061,
+			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+			bookTitle: "목련 만두",
+			authorName: "배고픈 사자",
+		},
+		{
+			bookIsbn: 9788901269061,
+			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+			bookTitle: "목련 만두",
+			authorName: "배고픈 사자",
+		},
+		{
+			bookIsbn: 9788901269061,
+			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+			bookTitle: "목련 만두",
+			authorName: "배고픈 사자",
+		},
+		{
+			bookIsbn: 9788901269061,
+			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+			bookTitle: "목련 만두",
+			authorName: "배고픈 사자",
+		},
+	];
+
+	return (
+		<div>
+			<ReturnNavigationBar title="좋아하는 도서" />
+			<Container paddingTop="56" paddingLeft="16" paddingRight="16">
+				<MyLikeHeading count={count} />
+			</Container>
+			<BookList books={books} />
+		</div>
+	);
 }
 
 export default MyLike;
