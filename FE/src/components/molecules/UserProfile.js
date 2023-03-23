ea24e8c176ebc62a1b9b45ac9aed25256b6bd5f1
aeebbr <aeebbr@gmail.com>
2023-03-23 22:08:47 +0900

    Feat: Modify UserProfile에 UserProfileImage 적용하여 프사 컴포넌트 분리

--- FE/src/components/molecules/UserProfile.js
+++ FE/src/components/molecules/UserProfile.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import { Span } from "../../styles/common/TextsStyle";
 import { ProfileContainer } from "../../styles/Sentence/PostPreviewStyle";
+import UserProfileImage from "../atoms/UserProfileImage";
 
 function UserProfile({ nickname, profileImg }) {
 	return (
 		<ProfileContainer>
-			<img src={profileImg} />
+			<UserProfileImage img={profileImg} size="sm" />
 			<Span weight="600" marginLeft="16">
 				{nickname}
 			</Span>
