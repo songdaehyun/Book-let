bc50dad23a374e4ed9e2c0af479466473b700799
aeebbr <aeebbr@gmail.com>
2023-03-23 17:30:43 +0900

    Design: Add 좋아하는 도서 빈 화면 마크업

--- FE/src/components/pages/Mypage/MyLike.js
+++ FE/src/components/pages/Mypage/MyLike.js
@@ -6,34 +6,52 @@ import MyLikeHeading from "../../atoms/Mypage/MyLikeHeading";
 import ReturnNavigationBar from "../../molecules/Bar/ReturnNavigationBar";
 import BookList from "../../organisms/Book/BookList";
 
+import useArr from "../../../hooks/useArr";
+import Empty from "../../molecules/Empty";
+
 function MyLike(props) {
 	const count = 15;
 	const books = [
-		{
-			bookIsbn: 9788901269061,
-			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
-			bookTitle: "목련 만두",
-			authorName: "배고픈 사자",
-		},
-		{
-			bookIsbn: 9788901269061,
-			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
-			bookTitle: "목련 만두",
-			authorName: "배고픈 사자",
-		},
-		{
-			bookIsbn: 9788901269061,
-			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
-			bookTitle: "목련 만두",
-			authorName: "배고픈 사자",
-		},
-		{
-			bookIsbn: 9788901269061,
-			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
-			bookTitle: "목련 만두",
-			authorName: "배고픈 사자",
-		},
+		// {
+		// 	bookIsbn: 9788901269061,
+		// 	bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+		// 	bookTitle: "목련 만두",
+		// 	authorName: "배고픈 사자",
+		// },
+		// {
+		// 	bookIsbn: 9788901269061,
+		// 	bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+		// 	bookTitle: "목련 만두",
+		// 	authorName: "배고픈 사자",
+		// },
+		// {
+		// 	bookIsbn: 9788901269061,
+		// 	bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+		// 	bookTitle: "목련 만두",
+		// 	authorName: "배고픈 사자",
+		// },
+		// {
+		// 	bookIsbn: 9788901269061,
+		// 	bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+		// 	bookTitle: "목련 만두",
+		// 	authorName: "배고픈 사자",
+		// },
 	];
+
+	const isArrEmpty = useArr();
+
+	const emptyInfo = {
+		title: "좋아하는 도서가 없어요",
+		subTitle: (
+			<>
+				관심 있거나 읽으신 도서에
+				<br />
+				좋아요를 남겨주세요!
+			</>
+		),
+		buttonLabel: "도서 탐색하러 가기",
+		path: "/book",
+	};
 
 	return (
 		<div>
@@ -41,7 +59,16 @@ function MyLike(props) {
 			<Container paddingTop="56" paddingLeft="16" paddingRight="16">
 				<MyLikeHeading count={count} />
 			</Container>
-			<BookList books={books} />
+			{isArrEmpty(books) ? (
+				<Empty
+					title={emptyInfo.title}
+					subTitle={emptyInfo.subTitle}
+					buttonLabel={emptyInfo.buttonLabel}
+					path={emptyInfo.path}
+				/>
+			) : (
+				<BookList books={books} />
+			)}
 		</div>
 	);
 }
