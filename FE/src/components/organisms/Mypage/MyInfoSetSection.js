a1ab7a0185e970b498b65715e72a4d4481c15a41
aeebbr <aeebbr@gmail.com>
2023-03-23 14:34:49 +0900

    Feat: Modify 감성 태그 수정 url path 수정

--- FE/src/components/organisms/Mypage/MyInfoSetSection.js
+++ FE/src/components/organisms/Mypage/MyInfoSetSection.js
@@ -9,7 +9,7 @@ function MyInfoSetSection(props) {
 		<Container marginTop="56" paddingLeft="16" paddingRight="16">
 			<Text size="14" weight="600" marginBottom="16" color="var(--gray-500)">회원 정보</Text>
 			<MoreBar title="회원 정보 수정" path="info/edit" />
-			<MoreBar title="감성 태그 수정" path="emotion/edit" />
+			<MoreBar title="감성 태그 수정" path="tag/edit" />
 		</Container>
 	);
 }
