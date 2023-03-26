64099cbbe8e14efb247b4c213d7ef01cbc98c67d
aeebbr <aeebbr@gmail.com>
2023-03-26 17:42:46 +0900

    Feat: Add 회원가입 api 함수 구현

---
+++ FE/src/apis/joinApi.js
@@ -0,0 +1,16 @@
+import api from "./index";
+
+export const join = async (data) => {
+	try {
+        const res = await api({
+            method: 'POST',
+            url: `/auth/signup`, 
+            data: data
+        })
+
+        console.log(res)
+        return res;
+	} catch (err) {
+		console.log(err);
+	}
+};
