c2788001dec993ff937de8224fa719ad9c5e9c02
aeebbr <aeebbr@gmail.com>
2023-03-26 17:58:35 +0900

    Feat: Add 회원가입 시 필요 data에 대하여 리덕스 action, reducer 구현 및 root reducer에 등록

--- FE/src/reducer/join.js
+++ FE/src/reducer/join.js
@@ -1,25 +1,62 @@
-// reducers/counter.js
+export const SET_ID = "join/SET_ID";
+export const SET_NICKNAME = "join/SET_NICKNAME";
+export const SET_PW = "join/SET_PW";
+export const SET_EMAIL = "join/SET_EMAIL";
+export const SET_AGE = "join/SET_AGE";
+export const SET_GENDER = "join/SET_GENDER";
 
-// reducer가 많아지면 action상수가 중복될 수 있으니
-// 액션이름 앞에 파일 이름을 넣습니다.
-export const INCRESE = "COUNT/INCRESE";
+export const setId = (id) => ({ type: SET_ID, id });
+export const setNickname = (nickname) => ({ type: SET_NICKNAME, nickname });
+export const setPw = (pw) => ({ type: SET_PW, pw });
+export const setEmail = (email) => ({ type: SET_EMAIL, email });
+export const setAge = (age) => ({ type: SET_AGE, age });
+export const setGender = (gender) => ({ type: SET_GENDER, gender });
 
-export const increseCount = count => ({ type: INCRESE, count });
-
-const initalState = {
-  count: 0
+const initialState = {
+	id: "",
+	nickname: "",
+	pw: "",
+	email: "",
+	age: "",
+	gender: 0
 };
 
-const counter = (state = initalState, action) => {
-  switch (action.type) {
-    case INCRESE:
-      return {
-        ...state,
-        count: action.count
-      };
-
-    // default를 쓰지 않으면 맨처음 state에 count값이 undefined가 나옵니다 꼭! default문을 넣으세요
-    default:
-      return state;
-  }
+const join = (state = initialState, action) => {
+	switch (action.type) {
+		case SET_ID:
+			return {
+				...state,
+				id: action.id
+			};
+		case SET_NICKNAME:
+			return {
+				...state,
+				nickname: action.nickname
+			};
+		case SET_PW:
+			return {
+				...state,
+				pw: action.pw
+			};
+		case SET_EMAIL:
+			return {
+				...state,
+				email: action.email
+			};
+		case SET_AGE:
+			return {
+				...state,
+				age: action.age
+			};
+		case SET_GENDER:
+			return {
+				...state,
+				gender: action.gender
+			};
+		// default를 쓰지 않으면 맨처음 state에 count값이 undefined가 나옵니다 꼭! default문을 넣으세요
+		default:
+			return state;
+	}
 };
+
+export default join;
