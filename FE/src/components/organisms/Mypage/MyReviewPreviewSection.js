e00bcc293efcc43ac0a1ec3c77153f0ed8a4ec67
aeebbr <aeebbr@gmail.com>
2023-03-27 14:01:09 +0900

    Feat: Add 내 리뷰  조회 api call

--- FE/src/components/organisms/Mypage/MyReviewPreviewSection.js
+++ FE/src/components/organisms/Mypage/MyReviewPreviewSection.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getMyReview } from "../../../apis/userApi";
 import { Container } from "../../../styles/common/ContainingsStyle";
 
 import MoreBar from "../../molecules/Bar/MoreBar";
 import MyReviewPreveiwCardGroup from "../../molecules/Mypage/MyReviewPreveiwCardGroup";
 
 function MyReviewPreviewSection(props) {
-	const reviews = [
-		{
-			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
-			bookTitle: "목련 만두",
-			authorName: "백유연",
-			bookPublisher: "웅진주니어",
-			bookIsbn: 9788901269061,
-			reviewGrade: 4.5,
-			reviewContent: "꽃과 만두라니 너무 아름답지 않나요??",
-			createdDate: "2023-03-9T11:11:11",
-		},
-		{
-			bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
-			bookTitle: "목련 만두",
-			authorName: "백유연",
-			bookPublisher: "웅진주니어",
-			bookIsbn: 9788901269061,
-			reviewGrade: 4.5,
-			reviewContent: "꽃과 만두라니 너무 아름답지 않나요??",
-			createdDate: "2023-03-9T11:11:11",
-		},
-	];
+	const [reviews, setReviews] = useState();
+
+	// const reviews = [
+	// 	{
+	// 		bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+	// 		bookTitle: "목련 만두",
+	// 		authorName: "백유연",
+	// 		bookPublisher: "웅진주니어",
+	// 		bookIsbn: 9788901269061,
+	// 		reviewGrade: 4.5,
+	// 		reviewContent: "꽃과 만두라니 너무 아름답지 않나요??",
+	// 		createdDate: "2023-03-9T11:11:11",
+	// 	},
+	// 	{
+	// 		bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+	// 		bookTitle: "목련 만두",
+	// 		authorName: "백유연",
+	// 		bookPublisher: "웅진주니어",
+	// 		bookIsbn: 9788901269061,
+	// 		reviewGrade: 4.5,
+	// 		reviewContent: "꽃과 만두라니 너무 아름답지 않나요??",
+	// 		createdDate: "2023-03-9T11:11:11",
+	// 	},
+	// ];
+
+	useEffect(() => {
+		(async () => {
+			await getMyReview().then((res) => {
+				setReviews(res);
+			});
+		})();
+	}, []);
 
 	return (
 		<Container marginTop="36" paddingLeft="16" paddingRight="16">
