ec573a3eff7168017e204ce1aa47dfcd21563ab0
전동준 <jdznawa@naver.com>
2023-03-27 14:27:06 +0900

    FEAT : Django 서버 환경변수 추가(비밀 키, DB 비밀번호)
    Former-commit-id: 01e693ebb4fa61ab5fec91eb8c6e9d2661f1d515

--- BE/django/recommend/settings.py
+++ BE/django/recommend/settings.py
@@ -12,16 +12,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s#s2pg+ncmtv2l^zb7%trw)0k&%3r$4la$b6#f2rxxa8ek2vn0'
+SECRET_KEY = environ.Env('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bookdb',
         'USER': 'ssafy',
-        'PASSWORD': 'ssafy',
+        'PASSWORD': environ.Env('DB_PW'),
         'HOST': 'localhost',
         'PORT': "3306"
     }
