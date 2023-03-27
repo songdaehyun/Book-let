04ae537b89baccc442801131db54c02d3a5c4479
전동준 <jdznawa@naver.com>
2023-03-27 15:41:05 +0900

    FIX : 모델 변경에 따른 views.py의 image_recommend 함수 수정 중(미완성)'
    Former-commit-id: 1a1b32d5cf788e9551f56b773a14afbfa8d213ea

--- BE/django/bookcover/views.py
+++ BE/django/bookcover/views.py
@@ -3,8 +3,12 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from PIL import Image
 import pandas as pd
+import cv2
+import numpy as np
 from .train import bookcover_recommendation
 from tensorflow import keras
+from io import BytesIO
+import requests
 
 
 # import yolov5
@@ -17,12 +21,13 @@ model = keras.models.load_model(modelpath)
 
 @csrf_exempt    # API를 만드는 경우 csrf 인증을 끄는 게 좋다.(대신 API 키 등의 방식을 사용)
 def image_recommend(request):   # 예측 기능 수행
-    if request.method == 'POST':
+    if request.method == 'GET':
         # isbn이나 image 정보가 없는 경우 오류 반환
 
         # Get 요청에서 입력 데이터 추출
         # input_data 처리 결과가 유효하지 않으면 오류가 반환해야 함
-        input_data = request.POST.get('book_image')
+        input_image = list(request.GET.get('book_image').split(','))
+        input_isbn = list(request.GET.get('book_isbn').split(','))
 
         # image_spec = 이미지의 confidence 값을 df로 요약한 형태
         image_spec = pd.DataFrame(columns=[
@@ -37,27 +42,34 @@ def image_recommend(request):   # 예측 기능 수행
         ])
 
         # 요청한 값의 이미지 URL을 하나씩 조회
-        for imgidx in range(len(input_data)):
+        for imgidx in range(len(input_image)):
+            col_image = input_image[imgidx]     # 이미지 URL 주소
             # 각 URL 별 Image 데이터 불러오기
-            input_pixel = Image.open(input_data[imgidx])
+            req_result = requests.get(col_image)
+            input_pixel = np.asarray(
+                bytearray(req_result.content), dtype=np.uint8)
+            # 이미지 크기를 (300, 300)으로 재조정
+            input_image = cv2.imdecode(input_pixel, cv2.IMREAD_COLOR)
+            input_image = cv2.resize(input_image, (300, 300))
+
+            image_RGB = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+
+        # Reshaping RGB image to get following: (batch size, rows, columns, channels)
+            x_input_RGB = image_RGB.reshape(
+                1, image_RGB.shape[0], image_RGB.shape[1], 3).astype(np.float32)
+
             image_value = dict()                # 이미지 keyword의 confidence 값을 딕셔너리 형태로 정리
 
             # book_id, book_isbn, book_image 설정
             image_value["book_id"] = imgidx
-            image_value["book_isbn"] = input_data["book_isbn"][imgidx]
-            image_value["book_image"] = input_data["book_image"][imgidx]
+            image_value["book_isbn"] = input_isbn[imgidx]
+            image_value["book_image"] = col_image
 
-            # 모델로 카테고리 분류한 후 pandas DF로 전환
-            input_info = model(input_pixel)
-            input_info = input_info.pandas().xyxy[0]
+            # 모델로 카테고리 분류
+            input_info = model.predict(x_input_RGB)
 
-            # input_info를 구성하는 행렬을 종류별로 조회
-            for col in range(input_info.shape[0]):
-                feature_col = input_info.iloc[col]
-                # 딕셔너리에 {분류: 유사도} 형태로 저장
-                if not feature_col.get(feature_col):
-                    image_value[input_info.name[col]
-                                ] = input_info.confidence[col]
+            # 72 ~ 83번 줄 새로 바꿔야 함(모델 변경)
+            # 딕셔너리에 {분류: 유사도} 형태로 저장
 
             image_value = pd.DataFrame([image_value])
 
