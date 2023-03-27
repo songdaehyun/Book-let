dda51b648e938bbbbe2d7e892dae6d27057ed15b
전동준 <jdznawa@naver.com>
2023-03-27 14:42:29 +0900

    FIX: 환경설정 롤백
    Former-commit-id: 1700928023c526dc6a166130eab58436099a42bb

--- BE/django/recommend/settings.py
+++ BE/django/recommend/settings.py
@@ -12,21 +12,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.Env('DJANGO_KEY')
+SECRET_KEY = 'django-insecure-s#s2pg+ncmtv2l^zb7%trw)0k&%3r$4la$b6#f2rxxa8ek2vn0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -99,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bookdb',
         'USER': 'ssafy',
-        'PASSWORD': environ.Env('DB_PW'),
+        'PASSWORD': 'ssafy',
         'HOST': 'localhost',
         'PORT': "3306"
     }
