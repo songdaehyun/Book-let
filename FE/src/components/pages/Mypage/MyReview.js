d6a586291d11ed8aaa01f7215293b6d1e61353d9
aeebbr <aeebbr@gmail.com>
2023-03-27 14:04:06 +0900

    Feat: Add 내 리뷰 리스트  조회 api call

--- FE/src/components/pages/Mypage/MyReview.js
+++ FE/src/components/pages/Mypage/MyReview.js
@@ -6,30 +6,41 @@ import MyReviewList from "../../organisms/Mypage/MyReviewList";
 
 import useArr from "../../../hooks/useArr";
 import Empty from "../../molecules/Empty";
+import { getMyReview } from "../../../apis/userApi";
 
 function MyReview(props) {
-	const reviews = [
-		// {
-		// 	bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
-		// 	bookTitle: "목련 만두",
-		// 	authorName: "백유연",
-		// 	bookPublisher: "웅진주니어",
-		// 	bookIsbn: 9788901269061,
-		// 	reviewGrade: 4.5,
-		// 	reviewContent: "꽃과 만두라니 너무 아름답지 않나요??",
-		// 	createdDate: "2023-03-9T11:11:11",
-		// },
-		// {
-		// 	bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
-		// 	bookTitle: "목련 만두",
-		// 	authorName: "백유연",
-		// 	bookPublisher: "웅진주니어",
-		// 	bookIsbn: 9788901269061,
-		// 	reviewGrade: 4.5,
-		// 	reviewContent: "꽃과 만두라니 너무 아름답지 않나요??",
-		// 	createdDate: "2023-03-9T11:11:11",
-		// },
-	];
+	const [reviews, setReviews] = useState([]);
+
+	// const reviews = [
+	// {
+	// 	bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+	// 	bookTitle: "목련 만두",
+	// 	authorName: "백유연",
+	// 	bookPublisher: "웅진주니어",
+	// 	bookIsbn: 9788901269061,
+	// 	reviewGrade: 4.5,
+	// 	reviewContent: "꽃과 만두라니 너무 아름답지 않나요??",
+	// 	createdDate: "2023-03-9T11:11:11",
+	// },
+	// {
+	// 	bookImgPath: "http://image.yes24.com/goods/117222565/BACK/XL",
+	// 	bookTitle: "목련 만두",
+	// 	authorName: "백유연",
+	// 	bookPublisher: "웅진주니어",
+	// 	bookIsbn: 9788901269061,
+	// 	reviewGrade: 4.5,
+	// 	reviewContent: "꽃과 만두라니 너무 아름답지 않나요??",
+	// 	createdDate: "2023-03-9T11:11:11",
+	// },
+	// ];
+
+	useEffect(() => {
+		(async () => {
+			await getMyReview().then((res) => {
+				setReviews(res);
+			});
+		})();
+	}, []);
 
 	const isArrEmpty = useArr();
 
