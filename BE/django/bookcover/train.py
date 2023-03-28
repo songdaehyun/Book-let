48a7c59936d02d1385ff0bc04d2b272a91e16bca
전동준 <jdznawa@naver.com>
2023-03-28 13:07:40 +0900

    FEAT: 이미지 유사도 매칭 알고리즘 소스코드 추가
    Former-commit-id: b8b5b1704aefcaebe633c10a2380398b15d46583

--- BE/django/bookcover/train.py
+++ BE/django/bookcover/train.py
@@ -17,7 +17,7 @@ def bookcover_recommendation(result_dataframe):
     # 추천 리스트
     rec_list = []
 
-    # df의 행을 하나씩 읽으면서 유사도 검사
+    # df의 행을 하나씩 읽으면서 각 이미지마다 유사도 검사
     for col in range(result_dataframe.shape[0]):
         # 요청받은 이미지의 특성 정보 행
         input_data = result_dataframe.iloc[col]
@@ -29,14 +29,26 @@ def bookcover_recommendation(result_dataframe):
         # 사용자가 제출한 선호 추천 표지
         image_src1 = input_data["book_image"]
 
-        # DB에 저장된 연관 추천 표지
-        for idx in range(len(db_dataframe)):
-            # db에서 하나씩 꺼내서 유사도 비교?(이하 미완성)
-            image_src2 = db_dataframe.objects.filter()
+        # 두 이미지를 numpy array로 전환
+        origin_img1 = np.asarray(
+            bytearray(
+                requests.get(image_src1).content
+            ),
+            dtype=np.uint8
+        )
+        resize_img1 = cv2.resize(origin_img1, (300, 300))
 
-            origin_img1 = np.asarray(
-                bytearray(requests.get(image_src1).content), dtype=np.uint8)
-            resize_img1 = cv2.resize(origin_img1, (300, 300))
+        # score와 DB 인덱스를 DataFrake 형태로 정리
+        rec_series = pd.DataFrame(columns=[
+            "book_image",
+            "ssim_score"
+        ])
+
+        # DB에 저장된 책 표지와 일대일 유사도 비교
+        for idx in range(len(db_dataframe)):
+            ssim_value = dict()
+            # DB의 정보들 중 idx 행에 있는 book_image에 저장된 URL 반환
+            image_src2 = db_dataframe.iloc[idx]["book_image"]
 
             origin_img2 = np.asarray(
                 bytearray(requests.get(image_src2).content), dtype=np.uint8)
@@ -45,8 +57,21 @@ def bookcover_recommendation(result_dataframe):
             # score = 이미지 간의 유사도(-1 ~ 1 사이, 1에 가까울수록 유사도 높음)
             (score, diff) = ssim(resize_img1, resize_img2, full=True)
 
-    # 유사도 상위 N개 필터링 후 rec_list에 추가
-    rec_list.append()
+            ssim_value["ssim_score"] = score
+            ssim_value["book_image"] = image_src2
+
+            ssim_value = pd.DataFrame([ssim_value])
+
+            # score, 이미지 URL 값을 rec_series에 추가
+            rec_series = pd.concat([rec_series, ssim_value])
+
+        # score를 기준으로 상위 20개 필터링
+        # rec_list에 추가할 상위 20개 점수의 이미지
+        highscore = rec_series.nlargest(20, ["book_image"], keep='first')
+        highscore = highscore.values.tolist()
+
+        # 필터링 결과는 {책 번호: [추천 이미지 목록]} 형태로 rec_list에 추가
+        rec_list.append({col: highscore})
 
     # pkl 파일로 static 디렉토리에 저장
     return rec_list
@@ -64,12 +89,7 @@ def data_refine_progress(df):
     image_spec = pd.DataFrame(columns=[
         "book_isbn",
         "book_image",
-        "anger",
-        "disgust",
-        "fear",
-        "joy",
-        "sadness",
-        "surprise"
+        "feeling"
     ])
 
     for idx in range(df.shape[0]):
@@ -96,12 +116,7 @@ def data_refine_progress(df):
         # book_isbn, book_image 설정
         image_value["book_isbn"] = column_case['isbn']
         image_value["book_image"] = column_case['image']
-        image_value["anger"] = predict_result[0][0]
-        image_value["disgust"] = predict_result[0][1]
-        image_value["fear"] = predict_result[0][2]
-        image_value["joy"] = predict_result[0][3]
-        image_value["sadness"] = predict_result[0][4]
-        image_value["surprise"] = predict_result[0][5]
+        image_value["feeling"] = column_case["feeling"]
 
         # predict 결과를 anger~surprise에 추가
 
