a5edff645f8115d88a348ecf8c523c8f0e53bd92
임경민 <aeebbr@gmail.com>
2023-03-28 14:57:23 +0900

    Feat: Add 문장 추천 api call
    Former-commit-id: 019161754f76da999f9732423a0ce5a32e79446f

--- FE/src/components/pages/Sentence/RecomSentence.js
+++ FE/src/components/pages/Sentence/RecomSentence.js
@@ -7,9 +7,23 @@ import PreviewPostWithProfile from "../../organisms/Sentence/PreviewPostWithProf
 import { Container } from "../../../styles/common/ContainingsStyle";
 
 import BannerImg from "../../../assets/images/Banner/reco-sentence-banner.png";
-import LoopyImg from "../../../assets/images/dummy/loopy-img.png";
+// import LoopyImg from "../../../assets/images/dummy/loopy-img.png";
+import { getSentenceRecom } from "../../../apis/sentenceApi";
+import { initScrappedList, initSentence } from "../../../apis/init/initSentence";
 
 function RecomSentence(props) {
+	const uId = localStorage.getItem("userId");
+
+	const [recom, setRecom] = useState();
+
+	useEffect(() => {
+		(async () => {
+			await getSentenceRecom(uId)
+				.then(initScrappedList)
+				.then((res) => setRecom(res));
+		})();
+	}, []);
+
 	const bannerInfo = {
 		title: "추천 문장",
 		subTitle: (
@@ -22,40 +36,40 @@ function RecomSentence(props) {
 	};
 
 	// 더미 데이터
-	const posts = [
-		{
-			paragraphId: 1,
-			profileImg: LoopyImg,
-			nickname: "루피는 행복해",
-			content:
-				"이제는 안다. 우리가 계속 지는 한이 있더라도 선택해야만 하는 건 이토록 평범한 미래라는 것을. 그리고 포기하지 않는 한 그 미래가 다가올 확률은 100퍼센트에 수렴한다는 것을.",
-			bookTitle: "이토록 평범한 미래",
-			bookAuthor: "김연수",
-			createdDate: "2023.03.01",
-			commentCnt: 10,
-			scrapUserImgs: [LoopyImg, LoopyImg, LoopyImg],
-			scrapCnt: 10,
-			userScrapted: 1, // 1 이면 스크랩 함. 0이면 안 함.
-			paragraphColor: "#B88962",
-			paragraphPage: 145,
-		},
-		{
-			id: 2,
-			profileImg: LoopyImg,
-			nickname: "루피는 책이 좋아",
-			content:
-				"이제는 안다. 우리가 계속 지는 한이 있더라도 선택해야만 하는 건 이토록 평범한 미래라는 것을. 그리고 포기하지 않는 한 그 미래가 다가올 확률은 100퍼센트에 수렴한다는 것을.",
-			bookTitle: "이토록 평범한 미래",
-			bookAuthor: "김연수",
-			createdDate: "2023.03.01",
-			commentCnt: 10,
-			scrapUserImgs: [LoopyImg, LoopyImg, LoopyImg],
-			scrapCnt: 10,
-			userScrapted: 0, // 1 이면 스크랩 함. 0이면 안 함.
-			paragraphColor: "#769676",
-			paragraphPage: 145,
-		},
-	];
+	// const posts = [
+	// 	{
+	// 		paragraphId: 1,
+	// 		profileImg: LoopyImg,
+	// 		nickname: "루피는 행복해",
+	// 		content:
+	// 			"이제는 안다. 우리가 계속 지는 한이 있더라도 선택해야만 하는 건 이토록 평범한 미래라는 것을. 그리고 포기하지 않는 한 그 미래가 다가올 확률은 100퍼센트에 수렴한다는 것을.",
+	// 		bookTitle: "이토록 평범한 미래",
+	// 		bookAuthor: "김연수",
+	// 		createdDate: "2023.03.01",
+	// 		commentCnt: 10,
+	// 		scrapUserImgs: [LoopyImg, LoopyImg, LoopyImg],
+	// 		scrapCnt: 10,
+	// 		userScrapted: 1, // 1 이면 스크랩 함. 0이면 안 함.
+	// 		paragraphColor: "#B88962",
+	// 		paragraphPage: 145,
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		profileImg: LoopyImg,
+	// 		nickname: "루피는 책이 좋아",
+	// 		content:
+	// 			"이제는 안다. 우리가 계속 지는 한이 있더라도 선택해야만 하는 건 이토록 평범한 미래라는 것을. 그리고 포기하지 않는 한 그 미래가 다가올 확률은 100퍼센트에 수렴한다는 것을.",
+	// 		bookTitle: "이토록 평범한 미래",
+	// 		bookAuthor: "김연수",
+	// 		createdDate: "2023.03.01",
+	// 		commentCnt: 10,
+	// 		scrapUserImgs: [LoopyImg, LoopyImg, LoopyImg],
+	// 		scrapCnt: 10,
+	// 		userScrapted: 0, // 1 이면 스크랩 함. 0이면 안 함.
+	// 		paragraphColor: "#769676",
+	// 		paragraphPage: 145,
+	// 	},
+	// ];
 
 	return (
 		<div>
