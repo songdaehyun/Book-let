d455203f289d45e081c0fc114666665a98eab6e2
임경민 <aeebbr@gmail.com>
2023-03-28 10:31:44 +0900

    Feat: 문장 상세에 삭제 버튼 추가 및 삭제 api call

--- FE/src/components/organisms/Sentence/DetailPostOverview.js
+++ FE/src/components/organisms/Sentence/DetailPostOverview.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-import DetailSentence from "../../molecules/Sentence/DetailSentence";
 import ScrapToolbar from "../../molecules/Bar/ScrapToolbar";
+import DetailSentence from "../../molecules/Sentence/DetailSentence";
 import ProfileWithFollow from "../../molecules/Sentence/ProfileWithFollow";
 
 import useDate from "../../../hooks/useDate";
 
+import { deletePost } from "../../../apis/sentenceApi";
 import { Container } from "../../../styles/common/ContainingsStyle";
+import { Span, Text } from "../../../styles/common/TextsStyle";
 import { DetailPostDateBox } from "../../../styles/Sentence/DetailSentenceStyle";
-import { Text } from "../../../styles/common/TextsStyle";
 
 function DetailPostOverview({
 	nickname,
@@ -22,11 +23,20 @@ function DetailPostOverview({
 	title,
 	author,
 	cover,
+	sId,
 	content,
 	page,
-	color
+	color,
 }) {
 	const dateTimeSeparation = useDate();
+
+	const handleClickDelete = () => {
+		(async () => {
+			await deletePost(sId).then((res) => {
+				console.log(res);
+			});
+		})();
+	};
 
 	return (
 		<>
@@ -55,6 +65,7 @@ function DetailPostOverview({
 				</Container>
 				<DetailPostDateBox>
 					<Text size="14" color="var(--gray-500)">
+						<Span onClick={handleClickDelete}>삭제</Span>
 						{dateTimeSeparation(date)}
 					</Text>
 				</DetailPostDateBox>
