8362ae3105eb0deb2a98413555e448ff03ba87a0
임경민 <aeebbr@gmail.com>
2023-03-28 09:34:19 +0900

    Feat: Modify api call 시 size 인자값 변경

--- FE/src/components/organisms/Sentence/PreviewScrapSentence.js
+++ FE/src/components/organisms/Sentence/PreviewScrapSentence.js
@@ -11,33 +11,9 @@ import useArr from "../../../hooks/useArr";
 import Empty from "../../molecules/Empty";
 
 function PreviewScrapSentence(props) {
-	// 더미 데이터
-	// const sentences = [
-	// {
-	// 	paragraphId: 1,
-	// 	paragraphColor: "#9FA3D0",
-	// 	content:
-	// 		"이제는 안다. 우리가 계속 지는 한이 있더라도 선택해야만 하는 건 이토록 평범한 미래라는 것을. 그리고 포기하지 않는 한 그 미래가 다가올 확률은 100퍼센트에 수렴한다는 것을.",
-	// },
-	// {
-	// 	paragraphId: 2,
-	// 	paragraphColor: "#543466",
-	// 	content:
-	// 		"이제는 안다. 우리가 계속 지는 한이 있더라도 선택해야만 하는 건 이토록 평범한 미래라는 것을. 그리고 포기하지 않는 한 그 미래가 다가올 확률은 100퍼센트에 수렴한다는 것을.",
-	// },
-	// {
-	// 	paragraphId: 3,
-	// 	paragraphColor: "#B88962",
-	// 	content:
-	// 		"이제는 안다. 우리가 계속 지는 한이 있더라도 선택해야만 하는 건 이토록 평범한 미래라는 것을. 그리고 포기하지 않는 한 그 미래가 다가올 확률은 100퍼센트에 수렴한다는 것을.",
-	// },
-	// ];
-
 	const [sentences, setSentences] = useState();
 
 	const isArrEmpty = useArr();
-
-	const id = localStorage.getItem("userId");
 
 	const emptyInfo = {
 		title: "스크랩한 문장이 없어요",
@@ -54,7 +30,7 @@ function PreviewScrapSentence(props) {
 
 	useEffect(() => {
 		(async () => {
-			await getScrappedPost(1, 0, 2)
+			await getScrappedPost(1, 0, 3)
 				.then(initScrappedList)
 				.then((res) => setSentences(res));
 		})();
