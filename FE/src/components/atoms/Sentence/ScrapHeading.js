704835c534714346319c34d8b745b9b32202d882
임경민 <aeebbr@gmail.com>
2023-03-28 09:43:16 +0900

    Feat: Add 스크랩 수 api call

--- FE/src/components/atoms/Sentence/ScrapHeading.js
+++ FE/src/components/atoms/Sentence/ScrapHeading.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getScrapCount } from "../../../apis/sentenceApi";
 
 import { Span, Text } from "../../../styles/common/TextsStyle";
 
 function ScrapHeading({ posts }) {
+	const id = localStorage.getItem("userId");
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		(async () => {
+			await getScrapCount(id).then((res) => {
+				setCount(res);
+			});
+		})();
+	}, []);
+
 	return (
 		<Text color="var(--gray-500)" marginBottom="24">
 			<Span weight="bold" color="var(--primary-600)">
-				{posts?.length}
+				{count}
 			</Span>
 			개의 문장을 스크랩했어요
 		</Text>
