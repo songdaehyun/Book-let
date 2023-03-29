e22f4de0bb00ec72ce552453f08e43132c5ea5c1
임경민 <aeebbr@gmail.com>
2023-03-29 10:44:53 +0900

    Feat: Add 리뷰 api 응답 데이터 정제 함수 구현
    Former-commit-id: 01e8dfaeb7dd9c439548e9f851a75cb7aa8fca97

--- FE/src/apis/init/initBook.js
+++ FE/src/apis/init/initBook.js
@@ -1,17 +1,31 @@
 export const initBook = (raw) => {
 	return {
-		cover: raw.bookImgPath,
-		title: raw.bookTitle,
+		cover: raw?.bookImgPath,
+		title: raw?.bookTitle,
 		aId: "",
-		author: raw.authorName,
-		publisher: raw.bookPublisher,
-		genres: raw.genreNames,
-		description: raw.description,
-		isbn: raw.bookIsbn,
-		authorOtherBooks: raw.authorOtherBooks,
-		isLiked: raw.bookLike,
-		likeCnt: raw.likesNumber,
-		likeProfileImg: raw.liikesProfileImg,
-		rating: raw.bookScore,
+		author: raw?.authorName,
+		publisher: raw?.bookPublisher,
+		genres: raw?.genreNames,
+		description: raw?.description,
+		isbn: raw?.bookIsbn,
+		authorOtherBooks: raw?.authorOtherBooks,
+		isLiked: raw?.bookLike,
+		likeCnt: raw?.likesNumber,
+		likeProfileImg: raw?.liikesProfileImg,
+		rating: raw?.bookScore,
 	};
+};
+
+export const initReview = (raw) => {
+	return raw?.map((review) => {
+		return {
+			uId: review?.userInfo?.userId,
+			commentId: review?.reviewId,
+			nickname: review?.userInfo?.nickname,
+			img: review?.userInfo?.userImage,
+			content: review?.reviewContent,
+			rating: review?.reviewGrade,
+			date: review?.createdDate,
+		};
+	});
 };
