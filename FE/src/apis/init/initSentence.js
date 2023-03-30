196c0896e5a67d1218633116ee8022e3c2ef6959
임경민 <aeebbr@gmail.com>
2023-03-30 14:19:13 +0900

    Feat: Modify initSentenceList 속성 수정
    Former-commit-id: 960ae7561ec6ded9f0858b1fc17ddd90e25a3910

--- FE/src/apis/init/initSentence.js
+++ FE/src/apis/init/initSentence.js
@@ -55,25 +55,25 @@ export const initSentence = (raw) => {
 export const initSentenceList = (raw) => {
 	return raw?.map((sentence) => {
 		return {
-			nickname: sentence?.user?.nickname,
-			profileImg: sentence?.user?.userImage,
+			nickname: sentence?.userInfo?.nickname,
+			profileImg: sentence?.userInfo?.userImage,
 
-			isbn: sentence?.book?.bookIsbn,
-			title: sentence?.book?.bookTitle,
-			author: sentence?.book?.bookAuthor,
-			cover: sentence?.book?.bookImage,
+			isbn: sentence?.bookIsbn,
+			title: sentence?.bookTitle,
+			author: sentence?.bookAuthor,
+			cover: sentence?.bookImage,
 
-			sId: sentence?.paragraph?.paragraphId,
-			content: sentence?.paragraph?.paragraphContent,
-			page: sentence?.paragraph?.paragraphPage,
-			color: sentence?.paragraph?.paragraphColor,
-			date: sentence?.paragraph?.createdDate,
+			sId: sentence?.paragraphId,
+			content: sentence?.paragraphContent,
+			page: sentence?.paragraphPage,
+			color: sentence?.paragraphColor,
+			date: sentence?.createdDate,
 
 			isScraped: sentence?.scrapInfo?.userScrape,
 			scrapImgs: sentence?.scrapInfo?.scrapUserImages,
 			scrapCount: sentence?.scrapInfo?.scrapCount,
 
-			commentCnt: sentence?.paragraphId?.commentCnt,
+			commentCnt: sentence?.commentCnt,
 		};
 	});
 };
