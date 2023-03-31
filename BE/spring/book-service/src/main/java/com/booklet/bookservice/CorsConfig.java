6a9ae7026abbcf0cf962c3b24de7bfb88ffd1d08
김지희 <cjg05034@naver.com>
2023-03-31 15:20:02 +0900

    Feat : Add corsFilter
    Former-commit-id: 13ff01debf1d2a8bd286a1414132a186b407ab35

---
+++ BE/spring/book-service/src/main/java/com/booklet/bookservice/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.booklet.bookservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        List<String> originlst = new ArrayList<>(List.of(
+                "http://localhost:3000",
+                "https://localhost:3000",
+                "http://j8b306.p.ssafy.io:3000",
+                "https://j8b306.p.ssafy.io:3000"
+        ));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(originlst);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+//        config.addExposedHeader(JwtProperties.HEADER_STRING);  // 헤더 보이게 설정하기
+
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
+}
