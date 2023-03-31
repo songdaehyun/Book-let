7d06d022c9ca4becee21f7a0ffa1bad693c94ab2
임경민 <aeebbr@gmail.com>
2023-03-31 12:41:14 +0900

    Feat: Modify Comment의 삭제 버튼 클릭 시 삭제 모달 구현
    Former-commit-id: eb59e24e7b7f709f88433bb304e89a06be65df37

--- FE/src/components/molecules/Comment.js
+++ FE/src/components/molecules/Comment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { deleteReview } from "../../apis/BookApi";
 import { deleteComment } from "../../apis/sentenceApi";
@@ -14,11 +14,23 @@ import { CommentBox } from "../../styles/common/CommonStyle";
 import { Text } from "../../styles/common/TextsStyle";
 import ReplyTextButton from "../atoms/Button/ReplyTextButton";
 import RatingLabel from "../atoms/RatingLabel";
+import ModalLayer from "../organisms/ModalLayer";
 
 function Comment({ comment, type, getCommentApiCall }) {
 	const { sId } = useParams();
 
 	const dateTimeSeparation = useDate();
+	const [isOpen, setIsOpen] = useState(false);
+
+	const openPopup = () => {
+		setIsOpen(true);
+	};
+
+	const handleClickDelete = () => {
+		setIsOpen(false);
+
+		type === "댓글" ? deleteCommentApiCall() : type === "리뷰" && deleteReviewApiCall();
+	};
 
 	const isMy = () => {
 		if (comment.uId === localStorage.getItem("userId")) {
@@ -49,52 +61,55 @@ function Comment({ comment, type, getCommentApiCall }) {
 	};
 
 	return (
-		<CommentBox>
-			<img
-				src={comment.img ? comment.img : defaultImg}
-				alt="Profile of the user who commented"
-			/>
+		<>
+			<CommentBox>
+				<img
+					src={comment.img ? comment.img : defaultImg}
+					alt="Profile of the user who commented"
+				/>
 
-			<div>
-				<CommentHeadingBox>
-					<Text weight="600">{comment?.nickname}</Text>
+				<div>
+					<CommentHeadingBox>
+						<Text weight="600">{comment?.nickname}</Text>
+						{type === "review" ? (
+							<RatingLabel rating={parseInt(comment?.rating)} />
+						) : (
+							<CommentDateBox>
+								<Text size="14" color="var(--gray-500)">
+									{dateTimeSeparation(comment?.date)}
+								</Text>
+							</CommentDateBox>
+						)}
+					</CommentHeadingBox>
+					<Text marginBottom="12">{comment?.content}</Text>
 					{type === "review" ? (
-						<RatingLabel rating={parseInt(comment?.rating)} />
+						<>
+							{isMy && <TextBtn onClick={openPopup}>삭제</TextBtn>}
+							<CommentDateBox>
+								<Text size="14" color="var(--gray-500)">
+									{dateTimeSeparation(comment?.date)}
+								</Text>
+							</CommentDateBox>
+						</>
 					) : (
-						<CommentDateBox>
-							<Text size="14" color="var(--gray-500)">
-								{dateTimeSeparation(comment?.date)}
-							</Text>
-						</CommentDateBox>
+						<div>
+							<CommentBtnBox>
+								<ReplyTextButton label="답글쓰기" />
+								{isMy && <TextBtn onClick={openPopup}>삭제</TextBtn>}
+							</CommentBtnBox>
+						</div>
 					)}
-				</CommentHeadingBox>
-				<Text marginBottom="12">{comment?.content}</Text>
-				{type === "review" ? (
-					<CommentDateBox>
-						<Text size="14" color="var(--gray-500)">
-							{dateTimeSeparation(comment?.date)}
-						</Text>
-					</CommentDateBox>
-				) : (
-					<div>
-						<CommentBtnBox>
-							<ReplyTextButton label="답글쓰기" />
-							{isMy && (
-								<TextBtn
-									onClick={
-										type === "댓글"
-											? deleteCommentApiCall
-											: type === "리뷰" && deleteReviewApiCall
-									}
-								>
-									삭제
-								</TextBtn>
-							)}
-						</CommentBtnBox>
-					</div>
-				)}
-			</div>
-		</CommentBox>
+				</div>
+			</CommentBox>
+			<ModalLayer
+				isOpen={isOpen}
+				setIsOpen={setIsOpen}
+				title="삭제하시겠습니까?"
+				leftLabel="취소"
+				rightLabel="삭제"
+				action={handleClickDelete}
+			/>
+		</>
 	);
 }
 
