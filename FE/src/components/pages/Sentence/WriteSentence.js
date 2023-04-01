fb80367b4697a08de7c9624004d51e6b989bc059
aeebbr <aeebbr@gmail.com>
2023-04-01 14:19:34 +0900

    Feat: Modify 문장 등록 유효성 패스해야 등록 버튼 색 적용, api call
    Former-commit-id: 27520f9cc2cf9b825cbb0831d068a0b316ecfd52

--- FE/src/components/pages/Sentence/WriteSentence.js
+++ FE/src/components/pages/Sentence/WriteSentence.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import ActionsNavigationBar from "../../molecules/Bar/ActionsNavigationBar";
@@ -14,6 +14,20 @@ function WriteSentence() {
 	const [content, setContent] = useState("");
 	const [background, setBackground] = useState("#FEEB60");
 	const [page, setPage] = useState("");
+	const [isSenetenceValid, setIsSenetenceValid] = useState(false);
+	const [nextColor, setNextColor] = useState("var(--gray-300");
+
+	useEffect(() => {
+		if (selectedBook !== undefined && content !== "" && page !== "") {
+			setIsSenetenceValid(true);
+		} else {
+			setIsSenetenceValid(false);
+		}
+	}, [selectedBook, content, page]);
+
+	useEffect(() => {
+		setNextColor(isSenetenceValid ? "var(--primary-600)" : "var(--gray-300)");
+	}, [isSenetenceValid]);
 
 	const handleClickPre = () => {
 		navigate("/");
@@ -28,13 +42,16 @@ function WriteSentence() {
 			userId: uId,
 		};
 
-		(async () => {
-			await createPost(data).then((res) => {
-				if (res.status === 201) {
-					navigate(`/sentence/${res.data.id}`);
-				}
-			});
-		})();
+		// 유효성 패스해야 api call
+		if (isSenetenceValid) {
+			(async () => {
+				await createPost(data).then((res) => {
+					if (res?.status === 201) {
+						navigate(`/sentence/${res.data.id}`);
+					}
+				});
+			})();
+		}
 	};
 
 	return (
@@ -43,6 +60,7 @@ function WriteSentence() {
 				pre="취소"
 				title="문장 쓰기"
 				next="확인"
+				nextColor={nextColor}
 				handleClickPre={handleClickPre}
 				handleClickNext={handleClickNext}
 			/>
