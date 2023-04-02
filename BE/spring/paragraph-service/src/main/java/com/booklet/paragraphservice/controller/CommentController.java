15057ffc938ea4a77878f16cfe27bda420c12d94
김지희 <cjg05034@naver.com>
2023-04-02 20:33:06 +0900

    Feat : Add 댓글 수정 및 삭제 기능
    Former-commit-id: e7bb7a33e92678c308baf59f6ccf4211c4361493

--- BE/spring/paragraph-service/src/main/java/com/booklet/paragraphservice/controller/CommentController.java
+++ BE/spring/paragraph-service/src/main/java/com/booklet/paragraphservice/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.booklet.paragraphservice.controller;
 import com.booklet.paragraphservice.dto.comment.CommentCreateReq;
 import com.booklet.paragraphservice.dto.comment.CommentDto;
 import com.booklet.paragraphservice.dto.comment.CommentListDto;
+import com.booklet.paragraphservice.dto.comment.CommentUpdateReq;
 import com.booklet.paragraphservice.entity.Paragraph;
 import com.booklet.paragraphservice.service.CommentService;
 import com.booklet.paragraphservice.service.ParagraphService;
@@ -27,40 +28,60 @@ public class CommentController {
     private final UserService userService;
     private final ParagraphService paragraphService;
 
-   /**
-    * 댓글 작성
-    *
-    * */
-    @PostMapping 
+    /**
+     * 댓글 작성
+     */
+    @PostMapping
     public ResponseEntity createComment(@RequestBody CommentCreateReq request) throws Exception {
         CommentDto commentDto = new CommentDto(request);
 //        log.info("HEEEEEEEERRRRRR : {}", commentDto.getCommentContent());
         commentDto.setUser(userService.getUser(request.getUserId()));
         commentDto.setParagraph(paragraphService.findParagraphEntity(request.getParagraphId()));
         Long commentId = commentService.saveComment(commentDto);
-        if(commentId>0L)   return new ResponseEntity("success", HttpStatus.ACCEPTED);
+        if (commentId > 0L) return new ResponseEntity("success", HttpStatus.ACCEPTED);
         return new ResponseEntity("fail", HttpStatus.BAD_REQUEST);
     }
-    
-    /**댓글 목록 조회*/
+
+    /**
+     * 댓글 목록 조회
+     */
     @GetMapping("/{paragraphId}")
-    public ResponseEntity getComments(@PathVariable Long paragraphId) throws Exception{
+    public ResponseEntity getComments(@PathVariable Long paragraphId) throws Exception {
         HashMap<String, Object> result = new HashMap<>();
-        try{
+        try {
             Paragraph paragraph = paragraphService.findParagraphEntity(paragraphId);
             List<CommentListDto> comments = commentService.findComments(paragraph);
-            if(comments.size()>0){
+            if (comments.size() > 0) {
                 result.put("comments", comments);
                 result.put("message", "success");
-            }else{
+            } else {
                 result.put("message", "no comments");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             result.put("message", "fail");
             return new ResponseEntity(result, HttpStatus.BAD_REQUEST);
 
         }
         return new ResponseEntity(result, HttpStatus.ACCEPTED);
+    }
 
+    /**
+     * 댓글 수정
+     **/
+    @PutMapping
+    public ResponseEntity updateComment(@RequestBody CommentUpdateReq request) throws Exception {
+        HashMap<String, Object> result = new HashMap<>();
+        if(commentService.updateComment(request.getCommentId(), request.getCommentContent())){
+            result.put("message", "success");
+        }else result.put("message","fail");
+        return new ResponseEntity(result, HttpStatus.ACCEPTED);
+    }
+
+    /** 댓글 삭제 **/
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity deleteComment(@PathVariable Long commentId) throws Exception{
+        if(commentService.deleteComment(commentId))  return new ResponseEntity("success", HttpStatus.ACCEPTED);
+
+        return new ResponseEntity("fail", HttpStatus.BAD_REQUEST);
     }
 }
