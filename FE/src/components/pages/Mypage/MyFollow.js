0bd6aac9031c0eac595ede9a72b934e497ce8df7
aeebbr <aeebbr@gmail.com>
2023-04-02 17:44:09 +0900

    Feat: Add  팔로우 조회 api call
    Former-commit-id: a3bfbda848e057f86ed4e1f5482edcb56ce0f9ba

--- FE/src/components/pages/Mypage/MyFollow.js
+++ FE/src/components/pages/Mypage/MyFollow.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import ReturnNavigationBar from "../../molecules/Bar/ReturnNavigationBar";
 import FollowTab from "../../molecules/Tab/FollowTab";
@@ -6,16 +6,32 @@ import FollowTab from "../../molecules/Tab/FollowTab";
 import { Container } from "../../../styles/common/ContainingsStyle";
 import MyFollowingTab from "../../organisms/Mypage/MyFollowingTab";
 import MyFollowerTab from "../../organisms/Mypage/MyFollowerTab";
+import { getFollow } from "../../../apis/userApi";
 
 function MyFollow(props) {
 	const [selectedItem, setSelectedItem] = useState(1);
+	const [followings, setFollowings] = useState([]);
+	const [followers, setFollowers] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			await getFollow().then((res) => {
+				setFollowings(res?.followings);
+				setFollowers(res?.followers);
+			});
+		})();
+	}, []);
 
 	return (
 		<div>
 			<ReturnNavigationBar title={selectedItem === 1 ? "팔로잉 목록" : "팔로워 목록"} />
 			<Container paddingTop="75" paddingLeft="16" paddingRight="16">
 				<FollowTab selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
-				{selectedItem === 1 ? <MyFollowingTab /> : <MyFollowerTab />}
+				{selectedItem === 1 ? (
+					<MyFollowingTab users={followings} />
+				) : (
+					<MyFollowerTab users={followers} />
+				)}
 			</Container>
 		</div>
 	);
