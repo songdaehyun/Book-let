c29ada07345977287e4c534895e5a9f915689e8d
김지희 <cjg05034@naver.com>
2023-04-02 17:34:34 +0900

    Feat : Modify Cors Config
    Former-commit-id: 8a7e50acd1a939213d9168a63415be7796c4b46d

--- BE/spring/book-service/src/main/java/com/booklet/bookservice/CorsConfig.java
+++ BE/spring/book-service/src/main/java/com/booklet/bookservice/CorsConfig.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     /**
      * CORS 설정, 허락된 URL, 허락된 Origin
+     *
      * @return
      */
     List<String> originlst = new ArrayList<>(List.of(
@@ -25,22 +28,20 @@ public class CorsConfig {
             "https://j8b306.p.ssafy.io"
     ));
     List<String> list = new ArrayList<>(List.of(
-            "GET","POST","PUT","OPTIONS","DELETE"
+            "GET", "POST", "PUT", "OPTIONS", "DELETE"
     ));
-    @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource corsConfigSource = new UrlBasedCorsConfigurationSource();
 
-        CorsConfiguration corsConfig = new CorsConfiguration();
-//        corsConfig.setAllowedHeaders(Arrays.asList(corsProperties.getAllowedHeaders().split(",")));
-        corsConfig.addAllowedHeader("*");
-        corsConfig.setAllowedMethods(list);
-        corsConfig.setAllowedOrigins(originlst);
-//        corsConfig.addAllowedOriginPattern("*");
-        corsConfig.setAllowCredentials(true);
-        corsConfig.setMaxAge(corsConfig.getMaxAge());
-
-        corsConfigSource.registerCorsConfiguration("/**", corsConfig);
-        return corsConfigSource;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000",
+                        "https://localhost:3000",
+                        "http://j8b306.p.ssafy.io:3000",
+                        "https://j8b306.p.ssafy.io:3000",
+                        "https://j8b306.p.ssafy.io")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+                .allowedHeaders("headers")
+                .maxAge(3000);
     }
+    
 }
