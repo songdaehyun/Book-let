875f0be7290660dd19126fa195870e44ed035950
aeebbr <aeebbr@gmail.com>
2023-04-04 00:58:03 +0900

    Design: Add 도서 미리보기의 MoreBar 스켈레톤 ui 적용
    Former-commit-id: e437fe3078574d77e8adba57dd514225c08ff2e9

--- FE/src/components/organisms/Book/PreviewBookSection.js
+++ FE/src/components/organisms/Book/PreviewBookSection.js
@@ -3,29 +3,38 @@ import useArr from "../../../hooks/useArr";
 import { Container } from "../../../styles/common/ContainingsStyle";
 
 import MoreBar from "../../molecules/Bar/MoreBar";
+import MoreBarSkeleton from "../../molecules/Bar/MoreBarSkeleton";
 import PreviewBookCoverList from "../../molecules/Book/PreviewBookCoverList";
 import PreviewBookSkeleton from "../../molecules/Book/PreviewBookSkeleton";
 import PreviewSwiperBook from "../../molecules/Book/PreviewSwiperBook";
 import Empty from "../../molecules/Empty";
 
-function PreviewBookSection({ title, books, path, emptyInfo, loading, error }) {
+function PreviewBookSection({ title, errTitle, books, path, emptyInfo, loading, error }) {
 	const isArrEmpty = useArr();
 
 	console.log(books);
 
 	return (
 		<Container marginTop="40" marginBottom="48">
-			<MoreBar title={title} path={path} />
-			{loading && <PreviewBookSkeleton />}
-			{error && (
-				<Empty
-					title={emptyInfo?.title}
-					subTitle={emptyInfo?.subTitle}
-					buttonLabel={emptyInfo?.buttonLabel}
-					path={emptyInfo?.path}
-				/>
+			{loading && (
+				<>
+					<MoreBarSkeleton />
+					<PreviewBookSkeleton />
+				</>
 			)}
-			{!isArrEmpty(books) &&
+			{error && (
+				<>
+					<MoreBar title={errTitle ? errTitle : title} path={path} />
+					<Empty
+						top={36}
+						title={emptyInfo?.title}
+						subTitle={emptyInfo?.subTitle}
+						buttonLabel={emptyInfo?.buttonLabel}
+						path={emptyInfo?.path}
+					/>
+				</>
+			)}
+			{!isArrEmpty(books) && <MoreBar title={title} path={path} /> &&
 				(books?.recommendType !== "bookCover" ? (
 					<PreviewSwiperBook books={books?.recommend || books} emptyInfo={emptyInfo} />
 				) : (
