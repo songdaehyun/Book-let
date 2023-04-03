e0edc9c3aaa7b118440ecfc7e5a60e56a11b5cc5
aeebbr <aeebbr@gmail.com>
2023-04-03 23:34:39 +0900

    Feat: Modify 도서 추천 api 응답을 로딩, 성공, 에러로 구분하기 위한 로직 수정
    Former-commit-id: 123bcacd0ff3ae2e0d8f8c57b21d57b88d268dc9

--- FE/src/components/organisms/Book/PreviewBookSection.js
+++ FE/src/components/organisms/Book/PreviewBookSection.js
@@ -1,19 +1,35 @@
 import React from "react";
+import useArr from "../../../hooks/useArr";
 import { Container } from "../../../styles/common/ContainingsStyle";
 
 import MoreBar from "../../molecules/Bar/MoreBar";
 import PreviewBookCoverList from "../../molecules/Book/PreviewBookCoverList";
 import PreviewSwiperBook from "../../molecules/Book/PreviewSwiperBook";
+import Empty from "../../molecules/Empty";
 
-function PreviewBookSection({ title, books, path }) {
+function PreviewBookSection({ title, books, path, emptyInfo, loading, error }) {
+	const isArrEmpty = useArr();
+
+	console.log(books);
+
 	return (
 		<Container marginTop="40" marginBottom="48">
 			<MoreBar title={title} path={path} />
-			{books?.recommendType !== "bookCover" ? (
-				<PreviewSwiperBook books={books?.recommend || books} />
-			) : (
-				<PreviewBookCoverList books={books?.recommend}></PreviewBookCoverList>
+			{loading && <>로딩 중</>}
+			{error && (
+				<Empty
+					title={emptyInfo?.title}
+					subTitle={emptyInfo?.subTitle}
+					buttonLabel={emptyInfo?.buttonLabel}
+					path={emptyInfo?.path}
+				/>
 			)}
+			{!isArrEmpty(books) &&
+				(books?.recommendType !== "bookCover" ? (
+					<PreviewSwiperBook books={books?.recommend || books} emptyInfo={emptyInfo} />
+				) : (
+					<PreviewBookCoverList books={books?.recommend}></PreviewBookCoverList>
+				))}
 		</Container>
 	);
 }
