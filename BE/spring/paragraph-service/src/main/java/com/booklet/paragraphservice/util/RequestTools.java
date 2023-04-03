5d318d50c8acd4c2066c1c1b8ced0387c23fc46d
김지희 <cjg05034@naver.com>
2023-04-03 14:27:15 +0900

    Feat : Add 장고 서버 호출 파일 준비...
    Former-commit-id: 752ee1cb2c5bb28e69e1c673d834a52323bc14d5

---
+++ BE/spring/paragraph-service/src/main/java/com/booklet/paragraphservice/util/RequestTools.java
@@ -0,0 +1,11 @@
+package com.booklet.paragraphservice.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class RequestTools {
+    RestTemplate restTemplate = new RestTemplate();
+
+
+}
