ba53015d898ea98a36b4490efd6248294cd90af5
aeebbr <aeebbr@gmail.com>
2023-04-04 00:25:04 +0900

    Design: Add 도서 미리보기 스켈레톤 ui 구현
    Former-commit-id: 87bf62bc18054207d30d5c13c4798465d6db72d1

--- FE/src/styles/Book/BookStyle.js
+++ FE/src/styles/Book/BookStyle.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const BookHeadingBox = styled.div`
 	display: flex;
@@ -70,5 +70,59 @@ export const BookSearchHeadingBox = styled.div`
 	button {
 		margin-bottom: 24px;
 		margin-left: 16px;
+	}
+`;
+
+const Loading = keyframes`
+	  0% {
+    transform: translateX(0);
+  }
+  50%,
+  100% {
+    transform: translateX(460px);
+  }
+  `;
+
+export const PreviewBookSkeletonBox = styled.div`
+	display: flex;
+	gap: 16px;
+	justify-content: space-between;
+`;
+
+export const PreviewBookSkeletonItemBox = styled.div`
+	width: 30%;
+
+	> div {
+		background-color: var(--gray-200);
+		border-radius: 4px;
+		position: relative;
+
+		overflow: hidden;
+		position: relative;
+
+		::before {
+			content: "";
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 8px;
+			height: 100%;
+
+			background-color: #f1f3f5;
+			/* background: linear-gradient(to right, #f2f2f2, #ddd, #f2f2f2); */
+			box-shadow: 0 0 16px 16px #f1f3f5;
+			/* box-shadow: 0 0 30px 30px rgba(255, 255, 255, 0.05); */
+
+			animation: ${Loading} 3s infinite linear;
+		}
+	}
+
+	> div:first-child {
+		height: 160px;
+		margin-bottom: 16px;
+	}
+
+	> div:nth-child(2) {
+		height: 16px;
 	}
 `;
